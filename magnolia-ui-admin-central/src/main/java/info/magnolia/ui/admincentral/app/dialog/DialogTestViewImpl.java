4d495895cc07a27b82ea3e4f6ecfc4d05a608b6b
Mikaël Geljic <mikael.geljic@magnolia-cms.com>
2012-05-31 10:38:18 +0200

    revert dialog test app view

--- magnolia-ui-admin-central/src/main/java/info/magnolia/ui/admincentral/app/dialog/DialogTestViewImpl.java
+++ magnolia-ui-admin-central/src/main/java/info/magnolia/ui/admincentral/app/dialog/DialogTestViewImpl.java
@@ -34,37 +34,31 @@
 package info.magnolia.ui.admincentral.app.dialog;
 
 import info.magnolia.ui.admincentral.app.AbstractAppView;
-import info.magnolia.ui.widget.actionbar.Actionbar;
 
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 
-
 /**
  * View implementation for the Pages app.
- * 
+ *
  * @version $Id$
  */
 @SuppressWarnings("serial")
 public class DialogTestViewImpl extends AbstractAppView<DialogTestPresenter> implements DialogTestView {
 
-    private final HorizontalLayout content = new HorizontalLayout();
+    private HorizontalLayout content = new HorizontalLayout();
 
     public DialogTestViewImpl() {
-
-        // TODO mgeljic 20120530 define api for assigning an actionbar to an app
-        content.addComponent(new Actionbar());
-
         Button dialog = new Button("Open Dialog", new DialogListener());
         content.addComponent(dialog);
         addTab(content, "Dialog");
     }
 
     /**
-     * Handler of Dialog events.
-     * 
+     * Handler of Dialog events. 
+     *
      */
     class DialogListener implements ClickListener {
 
