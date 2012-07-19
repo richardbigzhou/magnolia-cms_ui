536404b6f199bca2c2135cfa1b3e16f28a292e23
Tobias Mattsson <tobias.mattsson@magnolia-cms.com>
2012-07-19 18:17:39 +0200

    SCRUM-1365 inlined utility for MultipleBrowserWindowsApplicationServlet

--- magnolia-ui-vaadin-integration/src/main/java/info/magnolia/ui/vaadin/integration/servlet/MultipleBrowserWindowsApplicationServlet.java
+++ magnolia-ui-vaadin-integration/src/main/java/info/magnolia/ui/vaadin/integration/servlet/MultipleBrowserWindowsApplicationServlet.java
@@ -36,8 +36,10 @@ package info.magnolia.ui.vaadin.integration.servlet;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.util.UUID;
 
 import javax.servlet.ServletConfig;
@@ -51,7 +53,6 @@ import org.slf4j.LoggerFactory;
 import com.vaadin.Application;
 import com.vaadin.terminal.DownloadStream;
 import com.vaadin.terminal.gwt.server.AbstractApplicationServlet;
-import com.vaadin.terminal.gwt.server.AbstractCommunicationManager;
 import com.vaadin.terminal.gwt.server.CommunicationManager;
 import com.vaadin.ui.Window;
 
@@ -169,7 +170,7 @@ public class MultipleBrowserWindowsApplicationServlet extends MagnoliaIcePushSer
     @Override
     protected URL getApplicationUrl(HttpServletRequest request) throws MalformedURLException {
 
-        String path = RequestPathUtil.getContextPath(request) + RequestPathUtil.getServletPath(request);
+        String path = getContextPath(request) + getServletPath(request);
         String applicationId = getApplicationIdFromRequestPath(request);
         if (applicationId != null) {
             path = path + "/" + applicationId;
@@ -187,7 +188,7 @@ public class MultipleBrowserWindowsApplicationServlet extends MagnoliaIcePushSer
     }
 
     private String getApplicationIdFromRequestPath(HttpServletRequest request) {
-        String pathWithinServletMapping = RequestPathUtil.getPathWithinServletMapping(request);
+        String pathWithinServletMapping = getPathWithinServletMapping(request);
         if (pathWithinServletMapping.startsWith("/")) {
             pathWithinServletMapping = pathWithinServletMapping.substring(1);
         }
@@ -205,7 +206,7 @@ public class MultipleBrowserWindowsApplicationServlet extends MagnoliaIcePushSer
     }
 
     private String getPathAfterApplicationId(HttpServletRequest request) {
-        String pathWithinServletMapping = RequestPathUtil.getPathWithinServletMapping(request);
+        String pathWithinServletMapping = getPathWithinServletMapping(request);
         if (pathWithinServletMapping.startsWith("/")) {
             pathWithinServletMapping = pathWithinServletMapping.substring(1);
         }
@@ -273,5 +274,104 @@ public class MultipleBrowserWindowsApplicationServlet extends MagnoliaIcePushSer
     @Override
     protected String getRequestPathInfo(HttpServletRequest request) {
         return getPathAfterApplicationId(request);
+    }
+
+    private static final String DEFAULT_CHARACTER_ENCODING = "ISO-8859-1";
+
+    private static final String INCLUDE_REQUEST_URI_ATTRIBUTE = "javax.servlet.include.request_uri";
+    private static final String INCLUDE_CONTEXT_PATH_ATTRIBUTE = "javax.servlet.include.context_path";
+    private static final String INCLUDE_SERVLET_PATH_ATTRIBUTE = "javax.servlet.include.servlet_path";
+
+    /**
+     * Returns "" if the whole path has been matched by the servlet path.
+     */
+    private String getPathWithinServletMapping(HttpServletRequest request) {
+        String pathWithinApplication = getPathWithinApplication(request);
+        String servletPath = getServletPath(request);
+        if (pathWithinApplication.startsWith(servletPath)) {
+            pathWithinApplication = pathWithinApplication.substring(servletPath.length());
+        }
+        return pathWithinApplication;
+    }
+
+    private String getPathWithinApplication(HttpServletRequest request) {
+        String pathWithinApplication = getRequestUri(request);
+        String contextPath = getContextPath(request);
+        if (pathWithinApplication.startsWith(contextPath)) {
+            pathWithinApplication = pathWithinApplication.substring(contextPath.length());
+        }
+        return pathWithinApplication;
+    }
+
+    /**
+     * Returns the request uri for the request. If the request is an include it will return the uri being included. The
+     * returned uri is not decoded.
+     */
+    private String getRequestUri(HttpServletRequest request) {
+        if (request.getAttribute(INCLUDE_REQUEST_URI_ATTRIBUTE) != null) {
+            return (String) request.getAttribute(INCLUDE_REQUEST_URI_ATTRIBUTE);
+        }
+        return cleanRequestUri(decodeUri(request, request.getRequestURI()));
+    }
+
+    /**
+     * The servlet container is supposed to not include the part of the request uri following a semicolon but some
+     * containers (Jetty) leaves it in. This method removes it if it's there.
+     *
+     * @param uri a decoded request uri
+     */
+    private static String cleanRequestUri(String uri) {
+        int i = uri.indexOf(';');
+        if (i != -1) {
+            uri = uri.substring(0, i);
+        }
+        return uri;
+    }
+
+    /**
+     * Returns the decoded context path or empty if running as the root context. The context path always starts with a
+     * slash and never ends with a trailing slash.
+     */
+    private String getContextPath(HttpServletRequest request) {
+        String contextPath = (String) request.getAttribute(INCLUDE_CONTEXT_PATH_ATTRIBUTE);
+        if (contextPath == null) {
+            contextPath = request.getContextPath();
+        }
+        if ("/".equals(contextPath)) {
+            // Special case for includes in Jetty
+            contextPath = "";
+        }
+        return decodeUri(request, contextPath);
+    }
+
+    /**
+     * Returns the part of the request uri used to match the servlet being called. The servlet path has already been
+     * decoded by the servlet container.
+     */
+    private String getServletPath(HttpServletRequest request) {
+        String servletPath = (String) request.getAttribute(INCLUDE_SERVLET_PATH_ATTRIBUTE);
+        if (servletPath == null) {
+            servletPath = request.getServletPath();
+        }
+        if (servletPath.endsWith("/")) {
+            servletPath = servletPath.substring(servletPath.length() - 1);
+        }
+        return servletPath;
+    }
+
+    private static String decodeUri(HttpServletRequest request, String uri) {
+        try {
+            return URLDecoder.decode(uri, determineEncoding(request));
+        } catch (UnsupportedEncodingException e) {
+            return URLDecoder.decode(uri);
+        }
+    }
+
+    private static String determineEncoding(HttpServletRequest request) {
+        String characterEncoding = request.getCharacterEncoding();
+        if (characterEncoding == null) {
+            characterEncoding = DEFAULT_CHARACTER_ENCODING;
+        }
+        return characterEncoding;
     }
 }
