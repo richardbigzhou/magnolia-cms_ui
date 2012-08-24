273bde105cbf2d6c8fa46d3d6059d8a21cebca65
fgrilli <federico.grilli@magnolia-cms.com>
2012-08-24 15:32:06 +0200

    clean config and new custom save action

---
+++ magnolia-ui-app-contacts/src/main/java/info/magnolia/ui/app/contacts/dialog/action/SaveContactDialogAction.java
@@ -0,0 +1,85 @@
+/**
+ * This file Copyright (c) 2012 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.app.contacts.dialog.action;
+
+import info.magnolia.jcr.util.MetaDataUtil;
+import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.ui.admincentral.dialog.action.SaveDialogAction;
+import info.magnolia.ui.admincentral.dialog.action.SaveDialogActionDefinition;
+import info.magnolia.ui.model.action.ActionExecutionException;
+import info.magnolia.ui.vaadin.integration.jcr.JcrNodeAdapter;
+import info.magnolia.ui.widget.dialog.MagnoloaDialogPresenter.Presenter;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+/**
+ * A custom save action for contacts. It automatically creates a unique node name for a new contact.
+ *
+ */
+public class SaveContactDialogAction extends SaveDialogAction {
+
+    public SaveContactDialogAction(final SaveDialogActionDefinition definition, final Presenter presenter) {
+        super(definition, presenter);
+    }
+
+    @Override
+    public void execute() throws ActionExecutionException {
+        // First Validate
+        getPresenter().showValidation(true);
+        if (getPresenter().getView().isValid()) {
+            final JcrNodeAdapter itemChanged = (JcrNodeAdapter) getItem();
+
+            try {
+                final Node node = itemChanged.getNode();
+                generateUniqueNodeNameForContact(node);
+                MetaDataUtil.updateMetaData(node);
+                node.getSession().save();
+            } catch (final RepositoryException e) {
+                throw new ActionExecutionException(e);
+            }
+            getPresenter().getCallback().onSuccess(getDefinition().getName());
+
+        } else {
+        }
+    }
+
+    private void generateUniqueNodeNameForContact(final Node node) throws RepositoryException {
+        String firstName = node.getProperty("firstName").getString();
+        String lastName =  node.getProperty("lastName").getString();
+        String nodeName = firstName.charAt(0) + lastName;
+        node.getSession().move(node.getPath(), NodeUtil.combinePathAndName(node.getParent().getPath(), nodeName));
+    }
+
+}
