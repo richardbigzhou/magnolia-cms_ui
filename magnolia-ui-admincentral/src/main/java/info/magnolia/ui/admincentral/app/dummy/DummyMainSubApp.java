c1e4348b9515eb1c22b6f481d16c7c119a33ae6e
Mikaël Geljic <mikael.geljic@magnolia-cms.com>
2012-08-26 11:09:57 +0200

    SCRUM-1566 adding dummy app for all fake apps so that they can be launched

---
+++ magnolia-ui-admincentral/src/main/java/info/magnolia/ui/admincentral/app/dummy/DummyMainSubApp.java
@@ -0,0 +1,68 @@
+/**
+ * This file Copyright (c) 2012 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.admincentral.app.dummy;
+
+import info.magnolia.ui.framework.app.AbstractSubApp;
+import info.magnolia.ui.framework.app.AppContext;
+import info.magnolia.ui.framework.location.Location;
+import info.magnolia.ui.framework.view.View;
+
+import javax.inject.Inject;
+
+
+/**
+ * Sub app for the main tab in a dummy app.
+ */
+public class DummyMainSubApp extends AbstractSubApp {
+
+    private final DummyView dummyView;
+
+    private final AppContext appContext;
+
+    @Inject
+    public DummyMainSubApp(DummyView dummyView, AppContext appContext) {
+        this.dummyView = dummyView;
+        this.appContext = appContext;
+    }
+
+    @Override
+    public String getCaption() {
+        return appContext.getAppDescriptor().getLabel();
+    }
+
+    @Override
+    public View start(Location location) {
+        return dummyView;
+    }
+}
