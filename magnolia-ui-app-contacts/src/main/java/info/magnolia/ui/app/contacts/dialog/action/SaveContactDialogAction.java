3acb9e6079e66dea372d5e53816678365ddf9e25
fgrilli <federico.grilli@magnolia-cms.com>
2012-08-27 13:16:41 +0200

    QA remove unused module class

--- magnolia-ui-app-contacts/src/main/java/info/magnolia/ui/app/contacts/dialog/action/SaveContactDialogAction.java
+++ magnolia-ui-app-contacts/src/main/java/info/magnolia/ui/app/contacts/dialog/action/SaveContactDialogAction.java
@@ -44,7 +44,7 @@ import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
 /**
- * A custom save action for contacts. It automatically creates a unique node name for a new contact based on their <code>firstName</code> and <code>lastName</code>.
+ * A custom save action for contacts. It automatically creates a unique node name for a new contact based on their <code>firstName</code> and <code>lastName</code>
  * properties.
  */
 public class SaveContactDialogAction extends SaveDialogAction {
