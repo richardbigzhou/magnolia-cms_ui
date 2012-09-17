99510097929de8eed0d6b82e1e941f0180d12245
Tobias Mattsson <tobias.mattsson@magnolia-cms.com>
2012-09-17 15:20:48 +0200

    SCRUM-1588 added folder support for the contacts app

---
+++ magnolia-ui-app-contacts/src/main/java/info/magnolia/ui/app/contacts/action/AddFolderAction.java
@@ -0,0 +1,91 @@
+/**
+ * This file Copyright (c) 2012 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.app.contacts.action;
+
+import javax.inject.Named;
+import javax.jcr.Item;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import info.magnolia.cms.core.MetaData;
+import info.magnolia.cms.core.Path;
+import info.magnolia.context.MgnlContext;
+import info.magnolia.jcr.util.MetaDataUtil;
+import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.ui.admincentral.tree.action.RepositoryOperationAction;
+import info.magnolia.ui.framework.event.EventBus;
+
+/**
+ * Creates a folder either as a child if the node given is itself a folder or as a child of the nearest ancestor that
+ * is a folder.
+ */
+public class AddFolderAction extends RepositoryOperationAction<AddFolderActionDefinition> {
+
+    public AddFolderAction(AddFolderActionDefinition definition, Item item, @Named("admincentral") EventBus eventBus) {
+        super(definition, item, eventBus);
+    }
+
+    @Override
+    protected void onExecute(Item item) throws RepositoryException {
+
+        Node node = (Node) item;
+
+        node = findAncestorOfType(node, "mgnl:folder");
+        if (node == null) {
+            node = (Node) item.getAncestor(0);
+        }
+
+        String name = Path.getUniqueLabel(node.getSession(), node.getPath(), "untitled");
+        Node newNode = node.addNode(name, "mgnl:folder");
+        postProcessNode(newNode);
+    }
+
+    protected void postProcessNode(Node newNode) {
+        MetaData metaData = MetaDataUtil.getMetaData(newNode);
+
+        metaData.setAuthorId(MgnlContext.getUser().getName());
+        metaData.setCreationDate();
+        metaData.setModificationDate();
+    }
+
+    private Node findAncestorOfType(Node node, String nodeType) throws RepositoryException {
+        while (node.getDepth() > 0) {
+            if (NodeUtil.isNodeType(node, nodeType)) {
+                return node;
+            }
+            node = node.getParent();
+        }
+        return null;
+    }
+}
---
+++ magnolia-ui-app-contacts/src/main/java/info/magnolia/ui/app/contacts/column/ContactNameColumnFormatter.java
@@ -0,0 +1,88 @@
+/**
+ * This file Copyright (c) 2012 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.app.contacts.column;
+
+import javax.jcr.Item;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import com.vaadin.ui.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import info.magnolia.cms.core.MgnlNodeType;
+import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.ui.admincentral.column.AbstractColumnFormatter;
+import info.magnolia.ui.vaadin.integration.jcr.JcrItemAdapter;
+
+/**
+ * Column formatter that displays either the name of a contact or a folder.
+ *
+ * @see ContactNameColumnDefinition
+ */
+public class ContactNameColumnFormatter extends AbstractColumnFormatter<ContactNameColumnDefinition> {
+
+    private static final Logger log = LoggerFactory.getLogger(ContactNameColumnFormatter.class);
+
+    public ContactNameColumnFormatter(ContactNameColumnDefinition definition) {
+        super(definition);
+    }
+
+    @Override
+    public Object generateCell(Table source, Object itemId, Object columnId) {
+        JcrItemAdapter item = (JcrItemAdapter) source.getItem(itemId);
+        Item jcrItem = item.getJcrItem();
+        if (jcrItem != null && jcrItem.isNode()) {
+            Node node = (Node) jcrItem;
+
+            try {
+                if (NodeUtil.isNodeType(node, MgnlNodeType.NT_FOLDER)) {
+                    return node.getName();
+                }
+            } catch (RepositoryException e) {
+                log.warn("Unable to get name of folder for column", e);
+            }
+
+            try {
+                if (NodeUtil.isNodeType(node, "mgnl:contact")) {
+                    return node.getProperty("firstName").getString() + " " + node.getProperty("lastName").getString();
+                }
+            } catch (RepositoryException e) {
+                log.warn("Unable to get name of contact for column", e);
+            }
+
+        }
+        return "";
+    }
+}
