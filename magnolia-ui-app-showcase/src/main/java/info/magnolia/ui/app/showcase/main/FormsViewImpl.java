ac2d6544d253aed3f89a2795b5834c94826ff645
Samuli Penttila <samuli@vaadin.com>
2012-10-08 12:36:54 +0300

    SCRUM-1810 add button example to forms

--- magnolia-ui-app-showcase/src/main/java/info/magnolia/ui/app/showcase/main/FormsViewImpl.java
+++ magnolia-ui-app-showcase/src/main/java/info/magnolia/ui/app/showcase/main/FormsViewImpl.java
@@ -1,8 +1,12 @@
 package info.magnolia.ui.app.showcase.main;
 
 import com.vaadin.ui.Component;
+
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
+import com.vaadin.ui.NativeButton;
+
 import com.vaadin.ui.VerticalLayout;
 
 public class FormsViewImpl implements FormsView {
@@ -16,11 +20,31 @@ public class FormsViewImpl implements FormsView {
         layout.addComponent(new Label("The fields available in a Magnolia" +
         		" Form or Dialog. Configurable by repository or code."));
         
+        layout.addComponent(getButtonPreviews());
+        
+    }
+
+    private Layout getButtonPreviews() {
+        NativeButton sendButton = new NativeButton("Commit button");
+        sendButton.addStyleName("btn-dialog");
+        sendButton.addStyleName("btn-dialog-commit");
+        
+        NativeButton resetButton = new NativeButton("Cancel button");
+        resetButton.addStyleName("btn-dialog");
+        resetButton.addStyleName("btn-dialog-cancel");
+        
+        HorizontalLayout buttons = new HorizontalLayout();
+        buttons.setCaption("Buttons in a form");
+        buttons.addStyleName("buttons");
+        buttons.setSpacing(true);
+        buttons.addComponent(sendButton);
+        buttons.addComponent(resetButton);
+        return buttons;
     }
 
     @Override
     public Component asVaadinComponent() {
         return layout;
     }
-
+    
 }
