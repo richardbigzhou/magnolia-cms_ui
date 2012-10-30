326891e38fe94952d97c983bc2ae76a913f3b3d1
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2012-10-30 08:40:24 +0100

    QA streamlined tests - create DefaultImageProvider in setUp - remove testGetThumbnailFailsIfNoSizeIsSet as it is actually not failing when size is not set

--- magnolia-ui-model/src/test/java/info/magnolia/ui/model/thumbnail/DefaultImageProviderTest.java
+++ magnolia-ui-model/src/test/java/info/magnolia/ui/model/thumbnail/DefaultImageProviderTest.java
@@ -43,6 +43,7 @@ import info.magnolia.test.mock.MockWebContext;
 import info.magnolia.test.mock.jcr.MockSession;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.jcr.Node;
@@ -58,6 +59,7 @@ import static org.junit.Assert.assertNull;
 public class DefaultImageProviderTest {
     protected String workspaceName = "test";
     protected MockSession session;
+    private DefaultImageProvider imageProvider;
 
 
     @Before
@@ -69,6 +71,7 @@ public class DefaultImageProviderTest {
         webCtx.addSession(workspaceName, session);
         webCtx.setContextPath("/foo");
         MgnlContext.setInstance(webCtx);
+        imageProvider = new DefaultImageProvider();
     }
 
     @After
@@ -76,15 +79,18 @@ public class DefaultImageProviderTest {
         ComponentsTestUtil.clear();
         MgnlContext.setInstance(null);
         session = null;
+        imageProvider = null;
     }
 
 
     @Test
     public void testGetNonExistingParentNodeImagePath() throws Exception {
-        ImageProvider provider = new DefaultImageProvider();
+        // GIVEN - see setUp
 
-        final String result = provider.getThumbnailPath(workspaceName, null);
+        // WHEN
+        final String result = imageProvider.getThumbnailPath(workspaceName, null);
 
+        // THEN
         assertNull(result);
     }
 
@@ -105,14 +111,11 @@ public class DefaultImageProviderTest {
                         "/myNode/originalImage.size=1234";
 
         pie.createNodes(root, new ByteArrayInputStream(content.getBytes()));
-
-
-        ImageProvider provider = new DefaultImageProvider();
-
-        // WHEN
         final Node contactNode = root.getNode("myNode");
         final String imageNodeUuid = contactNode.getNode("originalImage").getIdentifier();
-        final String result = provider.getThumbnailPath(workspaceName, contactNode.getPath());
+
+        // WHEN
+        final String result = imageProvider.getThumbnailPath(workspaceName, contactNode.getPath());
 
         // THEN
         assertEquals("/foo/.imaging/thumbnail/test/"+ imageNodeUuid +"/MaxMustermann.png", result);
@@ -122,7 +125,6 @@ public class DefaultImageProviderTest {
     public void testGetPortraitPathByUuid() throws Exception {
         // GIVEN
         final PropertiesImportExport pie = new PropertiesImportExport();
-
         final Node root = session.getRootNode();
 
         final String content =
@@ -134,14 +136,11 @@ public class DefaultImageProviderTest {
                         "/myNode/originalImage.size=1234";
 
         pie.createNodes(root, new ByteArrayInputStream(content.getBytes()));
-
-
-        ImageProvider provider = new DefaultImageProvider();
-
-        // WHEN
         final Node contactNode = root.getNode("myNode");
         final String imageNodeUuid = contactNode.getNode("originalImage").getIdentifier();
-        final String result = provider.getPortraitPathByIdentifier(workspaceName, contactNode.getIdentifier());
+
+        // WHEN
+        final String result = imageProvider.getPortraitPathByIdentifier(workspaceName, contactNode.getIdentifier());
 
         // THEN
         assertEquals("/foo/.imaging/portrait/test/"+ imageNodeUuid +"/MaxMustermann.png", result);
@@ -151,9 +150,7 @@ public class DefaultImageProviderTest {
     public void testGetThumbnailPathWithoutFileName() throws Exception {
         // GIVEN
         final PropertiesImportExport pie = new PropertiesImportExport();
-
         final Node root = session.getRootNode();
-
 
         final String content =
                         "/myNode/originalImage.@type=mgnl:resource\n" +
@@ -163,51 +160,20 @@ public class DefaultImageProviderTest {
                         "/myNode/originalImage.size=1234";
 
         pie.createNodes(root, new ByteArrayInputStream(content.getBytes()));
-
-
-        ImageProvider provider = new DefaultImageProvider();
-
-        // WHEN
         final Node contactNode = root.getNode("myNode");
         final String imageNodeUuid = contactNode.getNode("originalImage").getIdentifier();
-        final String result = provider.getThumbnailPath(workspaceName, contactNode.getPath());
+
+        // WHEN
+        final String result = imageProvider.getThumbnailPath(workspaceName, contactNode.getPath());
 
         // THEN
         assertEquals("/foo/.imaging/thumbnail/test/"+ imageNodeUuid +"/myNode.png", result);
-    }
-
-    @Test(expected=PathNotFoundException.class)
-    public void testGetThumbnailFailsIfNoSizeIsSet() throws Exception {
-        // GIVEN
-        final PropertiesImportExport pie = new PropertiesImportExport();
-        final MockSession session  = new MockSession("test");
-        final Node root = session.getRootNode();
-
-        //notice the missing size property here
-        final String content =
-                "/myNode/thumbnail.@type=mgnl:resource\n" +
-                        "/myNode/thumbnail.fileName=MaxMustermann\n" +
-                        "/myNode/thumbnail.extension=gif\n" +
-                        "/myNode/thumbnail.jcr\\:data=binary:R0lGODlhUABrAPcAAGYAAOi2lOi9ne2thO2le+Wdc+y3kvGWZe2zjOfDpu+fc+iuivCabOajfOjBouerhOfGqeeabfCWaOixjuange+idu+pf2wJB2gCAumhdH0fGHMSDuSgeYwzKOCzlnEPC7d0YNaliuWWbHYWEZ1IOeuviYQpIKRLN+ungMuVfapiUZI4KsaLc6VaSt6LYrdjSZVAM3kZE4svJNyOaN2vkp5RRJtMP9WFYNaKZYEjG6VTQWoFBOGlgnobFcdzUr1yWMd5W7dxXOG3m+mpf4ksIqxYQpxDMtipjtijhsCBarhgRZE7MNOfheSPZOGTa+W4moElHcOEbI45L8+bg5I1J96rjMV8Ybt9aMyOc7x3YG8NCsFxVLNdRK9oVcBpS9OCXYw2LeaRZbxlSbx7Zs53Vc6Qdb9sT9CCYNKZfJpFNqdfTpRCNtJ/W6xeScl4VqFINbJmUcdvUIcuJOCbdc1+XLl4ZMaIb7VsV8mTfMOHcKpRO5hGOaZeT+SwkLBqV4YvJ6pfTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAAIEALAAAAABQAGsAAAj/AAMIHEiw4EADBgQiXJhQIcOHECNKnDjRoMWDFDNq3MjRAAIEFwM0xPiQYMeTCz9q/MgSpMiRIU3CLIiSocqWLT3i3Nky5kWVAlkuDCBUJ8+jSJMijelSqdOnUJMOGPBx6oAJWLNq3ar16ISoSUtMZWm1rNmzVheoXbtgAtu3akvI1RqXKgK0ePPq3ZsWrt+/cPkKHiwYsOG/DwgrXlyW7YPHkCNLlsy4Ml8LmDNbUDu5s+fIAxLr1YzZKunTqE8TWM2awOfXkIcMaUCBwgMLU2XjHlC6NevMvoMLH+47cm3bxyk0WM68+fLWzxuwlk68uvXrBJLXpq3c+XPs4MML/69Avrz58sm9O19NXTxr8qvLE6gQX/788+cV6N/Pf7/6/9K1V0EGBGZA32oKxFdAASI4MQMOOMwwwxlbmHEGDiLoh19/HHbYH4DqEUhfgSRWwJ8INwDhhRJ6nEACCSecsEQPG3SQRhE+uODhjjzqB2IGBZAoZIELFlmAEze8sAIUG3ywAwBQRiklBhqcQEcEPfbHwJZcdslAASEaGSSQYhapH4M3iLHCBhhI6eabUMrggwhedqlfnXhuuRyYZZbJ45E+rPABnIS+KYMXTeSp6JYSeMlcn34q0GcEM7ygQZuFZhqlBkok2mWjdYJ6wAGNjjoqpKgqEMGqrEbgwgs5YP+qqaZUikGqBKbmquuupqJaZqvAiuBDB7LOSisRcUigLK68NturrwsCK60bK1xg7LUAfHCCC6TyyqyzB0AbrbSrMvAFCRtgey2VSigLLrjQktuqCyewqe61F1BBxrvwQrpqAfJGIIKl96r7wRvc8ssrqgGz+kW1BWN7gQz7Lmvxt80y3LAI9UasbgxcXOwuv+ICHOwPRFjrMb4d3HCrws+WPC4OEK+MrQZbjAxzuDIXGcEPGtis7ghc8KszqT0b+cIIQmOrrai7MourxUkXScKgTRt7gRFhcJkrA8uOavEBzXFQAAcNmA3pDTKonLWmW3fttbKMiqysc2gvx0Hee/f//UIMxb5NaL6ecgmq4SKDWPaeJLgteKE7EOHCol7SfZziec/QgeOPw4lBDmxQjqd2l6vHARCXdq7pCGYwcKfrotf2GOmkzwED56q/+UERru+Ip2eyR8ZDFFAEnrubW2f5+pawCX9HuscXegEMWCqvgOsPrAUbD3DgHn2UE2dovX6HCQ/CCMZ/D8AFKxTgW5aHOVaGDE+q7+YGRXBgHYdcGcZD4/ZzUwyA4B4FWCQrcHlAG7AWQCjFAAftwU4FAuCBkGxlAT+AXgMxYIIZKIcAEayOTwrSByBoMIAf0EEBHoOCB6AABat54XBGWBAWpC6AGICCFURTlhdOBYYhpCFB/5AAwABuQAcNmAoP0fIACrRGiATJwg3VlwMrVIYAAsiiFreoxYLQAAsqmGLujtiAv8hFL1xMIxcJUoUiHk8DP2gLYMRilRIswCpqzKMAntDFFpywcxjoABYCgJX4vWUAekykADzgx+hpoQZVGAhXssISuCgykW34o+CoFISZDAQBXfnIW7LoAAdckotSzN0OetACJDDEIh/BCk9MScpSnlIAKQBD+oSmAT8g4SUJGQpTWFLKBJTymMikZRo9oAKmPQ4DLQiBTxDikwRY85rJzGYy7UCsx2kgCTSEyEEEcs1ymvOY53TAE7rgzLfBAA1phOJLAmDOetoTmw5IwRIEp/8FEHhAkSNEyD0HWs8Q2EBwNmDCLbdoEQNcEwIEHWgIauC9ghGBBQFYKEMNYk0IQDSi9qSBGhjosUAm4Z8a1WMAPMrSjyagpSztaDmFAIIeCG0EVxBCNlPKRZj69KcxTYAAQBC0lXEQDwLQphZtudCOArWlL4WqUKNgApthAKceKKYxlbrQp3o0ogIoA7qEFkgQhCCr2kTmLb3qUnNCwAFTUIEJLrBLdV1gA1LQwRimIIStppWpeXSrT+spgCkEoQMbqN/bLvCBHoBBBSwIwRP+ytUsCvan1vRAClQgBQ1UNGsXGIEJYNCCIESBCWilrDLvGVMHHCEFILDBHzZAV/uyYeCuPTDBGkqbAhokNa1alCo2j8CC2MLAsw180203oIEO1AAEKTjrb4+pxXN6gAksqEMNOuDZzyYXfBqQQg26MIbe/ra6xhRCCPKggj1w17vfHdwIcgADFSQhBUeobgiKa4O5xhe0OVgCH1jg2xDowAT2+u/jLiAHswJOwdG7QAwgTOEKW/jCGM6whjfM4Q57+MMgDrGIR0ziEpv4xChOsYpXzOIWu/jFMI6xjGdMY6EFBAA7\n" +
-                        "/myNode/thumbnail.jcr\\:mimeType=image/gif";
-
-        pie.createNodes(root, new ByteArrayInputStream(content.getBytes()));
-
-        DefaultImageProvider provider = new DefaultImageProvider();
-
-
-        // WHEN
-        final Node contactNode = root.getNode("myNode");
-
-        // THEN
-        contactNode.getProperty("size");
     }
 
     @Test
     public void testGetPortraitFromNonDefaultOriginalImageNodeName() throws Exception {
         // GIVEN
         final PropertiesImportExport pie = new PropertiesImportExport();
-
         final Node root = session.getRootNode();
 
         final String content =
@@ -219,19 +185,15 @@ public class DefaultImageProviderTest {
                         "/contact1/photo.size=1234";
 
         pie.createNodes(root, new ByteArrayInputStream(content.getBytes()));
+        imageProvider.setOriginalImageNodeName("photo");
 
-        DefaultImageProvider provider = new DefaultImageProvider();
-        provider.setOriginalImageNodeName("photo");
-
-
-        // WHEN
         final Node contactNode = root.getNode("contact1");
         final String imageNodeUuid = contactNode.getNode("photo").getIdentifier();
 
-        final String result = provider.getPortraitPath(workspaceName, contactNode.getPath());
+        // WHEN
+        final String result = imageProvider.getPortraitPath(workspaceName, contactNode.getPath());
 
         // THEN
         assertEquals("/foo/.imaging/portrait/test/"+ imageNodeUuid +"/MaxMustermann.png", result);
     }
-
 }
