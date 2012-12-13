richardbigzhou/magnolia-cms_ui

commit 86921142515bd9b02476faddb3f14e16728e560d

QA simplify by applying common pattern (compare constant with unkown value - not the other way around)

magnolia-ui-app-pages/src/main/java/info/magnolia/ui/app/pages/main/PagesMainSubApp.java
@@ -76,7 +76,7 @@ public class PagesMainSubApp extends AbstractContentSubApp implements PagesMainV
         actionbar.disable("duplicate");
 
         // actions disabled based on selection
-        if (getWorkbench().getSelectedItemId() == null || getWorkbench().getSelectedItemId().equals("/")) {
+        if ("/".equals(getWorkbench().getSelectedItemId())) {
             actionbar.disable("delete");
             actionbar.disable("preview");
             actionbar.disable("edit");

commit 69aaa2ee043085917dda71c54c00813da309232a

QA fixed regression introduced with 86921142515bd9b02476faddb3f14e16728e560d

magnolia-ui-app-pages/src/main/java/info/magnolia/ui/app/pages/main/PagesMainSubApp.java
@@ -69,7 +69,7 @@ public class PagesMainSubApp extends AbstractContentSubApp implements PagesMainV
         actionbar.disable("duplicate");
 
         // actions disabled based on selection
-        if ("/".equals(getWorkbench().getSelectedItemId())) {
+        if (getWorkbench().getSelectedItemId() == null || "/".equals(getWorkbench().getSelectedItemId())) {
             actionbar.disable("delete");
             actionbar.disable("preview");
             actionbar.disable("edit");