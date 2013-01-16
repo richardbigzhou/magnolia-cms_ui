3899eded00274837ecefaf685ebe0acd00717bb1
Jozef Chocholacek <jozef.chocholacek@magnolia-cms.com>
2013-01-16 14:10:12 +0100

    MGNLUI-560 : getExtraInstallTasks() method added to the version handler

--- magnolia-ui-legacy-adminInterface/src/main/java/info/magnolia/ui/legacy/admininterface/setup/AdminInterfaceLegacyModuleVersionHandler.java
+++ magnolia-ui-legacy-adminInterface/src/main/java/info/magnolia/ui/legacy/admininterface/setup/AdminInterfaceLegacyModuleVersionHandler.java
@@ -1,12 +1,31 @@
 package info.magnolia.ui.legacy.admininterface.setup;
 
 import info.magnolia.module.DefaultModuleVersionHandler;
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.RemoveNodeTask;
+import info.magnolia.module.delta.Task;
+
+import java.util.List;
 
 /**
- * This class is optional and lets you manager the versions of your module,
- * by registering "deltas" to maintain the module's configuration, or other type of content.
- * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * This version handler removes on install the old command definitions from the
+ * admin interface module (if installed).
  */
 public class AdminInterfaceLegacyModuleVersionHandler extends DefaultModuleVersionHandler {
+
+    @Override
+    protected List<Task> getExtraInstallTasks(InstallContext installContext) {
+        List<Task> tasks = super.getExtraInstallTasks(installContext);
+        tasks.add(new RemoveNodeTask("Remove default activate command",
+                "Removes the activate command definition from the default catalog (path /modules/adminInterface/commands/default/activate).",
+                "config", "/modules/adminInterface/commands/default/activate"));
+        tasks.add(new RemoveNodeTask("Remove default deactivate command",
+                "Removes the deactivate command definition from the default catalog (path /modules/adminInterface/commands/default/deactivate).",
+                "config", "/modules/adminInterface/commands/default/deactivate"));
+        tasks.add(new RemoveNodeTask("Remove website activate command",
+                "Removes the activate command definition from the website catalog (path /modules/adminInterface/commands/website/activate).",
+                "config", "/modules/adminInterface/commands/website/activate"));
+        return tasks;
+    }
 
 }
