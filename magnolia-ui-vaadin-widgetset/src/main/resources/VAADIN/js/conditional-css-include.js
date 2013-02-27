b86f5e98f99f06fa473ad3c02cb84dfdfc7e0b20
Mikaël Geljic <mikael.geljic@magnolia-cms.com>
2013-02-27 18:24:19 +0100

    MGNLUI-710 revised font settings so that bold is meaningful, dropped inconsistent font-weight workaround, also enhanced login screen with minor design adjustments and the SVG logo

--- magnolia-ui-vaadin-widgetset/src/main/resources/VAADIN/js/conditional-css-include.js
+++
@@ -1,5 +0,0 @@
-if (navigator.userAgent.toLowerCase().indexOf('webkit')>-1 || navigator.appName == 'Microsoft Internet Explorer') {
-    document.write('<link rel="stylesheet" type="text/css" href="../VAADIN/themes/admincentraltheme/css-conditional/fonts-heavy.css" />');
-}else{
-    document.write('<link rel="stylesheet" type="text/css" href="../VAADIN/themes/admincentraltheme/css-conditional/fonts-default.css" />');
-}
