f2803d4368f2a5c4e6faccd01a0711c27397dc59
Mikaël Geljic <mikael.geljic@magnolia-cms.com>
2013-04-10 11:30:20 +0200

    MGNLUI-1071 base view and presenter interfaces for status bar, plus default view impl.

---
+++ magnolia-ui-statusbar/src/main/java/info/magnolia/ui/statusbar/StatusBarViewImpl.java
@@ -0,0 +1,103 @@
+/**
+ * This file Copyright (c) 2013 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.statusbar;
+
+import java.util.Iterator;
+
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+
+/**
+ * The status bar view interface is implemented purely on the server-side - without custom widgets. In fact, it extends
+ * the @link{HorizontalLayout} directly, rather than using a vaadin @link{com.vaadin.ui.CustomComponent}, because this
+ * view impl is never exposed explicitly.
+ * Color change is ensured by toggling CSS preset styles.
+ */
+public class StatusBarViewImpl extends HorizontalLayout implements StatusBarView {
+
+    private static final String STYLE_NAME = "statusbar";
+
+    private String color;
+
+    public StatusBarViewImpl() {
+        removeStyleName("v-horizontallayout");
+        setStyleName(STYLE_NAME);
+    }
+
+    @Override
+    public void addComponent(Component c, Alignment align) {
+        int index;
+        // compute index based on requested alignment, first come first aligned
+        if (align.isLeft()) {
+            index = getFirstAvailableLeftIndex();
+        } else {
+            index = getFirstAvailableRightIndex();
+        }
+
+        addComponent(c, index);
+        setComponentAlignment(c, align);
+    }
+
+    private int getFirstAvailableLeftIndex() {
+        int index = 0;
+        Iterator<Component> it = getComponentIterator();
+        while (it.hasNext() && getComponentAlignment(it.next()).isLeft()) {
+            index++;
+        }
+        return index;
+    }
+
+    private int getFirstAvailableRightIndex() {
+        int index = 0;
+        Iterator<Component> it = getComponentIterator();
+        while (it.hasNext() && !getComponentAlignment(it.next()).isRight()) {
+            index++;
+        }
+        return index;
+    }
+
+    @Override
+    public void setColor(String colorStyleName) {
+        removeStyleName(this.color);
+        addStyleName(colorStyleName);
+        this.color = colorStyleName;
+    }
+
+    @Override
+    public Component asVaadinComponent() {
+        return this;
+    }
+
+}
