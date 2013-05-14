60423610871393800752093450ec01c4cb8ede62
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2013-05-14 14:07:32 +0200

    QA drop unused import

--- magnolia-ui-vaadin-integration/src/main/java/info/magnolia/ui/vaadin/integration/jcr/JcrItemUtil.java
+++ magnolia-ui-vaadin-integration/src/main/java/info/magnolia/ui/vaadin/integration/jcr/JcrItemUtil.java
@@ -35,10 +35,9 @@ package info.magnolia.ui.vaadin.integration.jcr;
 
 import info.magnolia.context.MgnlContext;
 
-import javax.jcr.Node;
 import javax.jcr.Item;
+import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
-import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 
 import org.apache.commons.lang.StringUtils;
