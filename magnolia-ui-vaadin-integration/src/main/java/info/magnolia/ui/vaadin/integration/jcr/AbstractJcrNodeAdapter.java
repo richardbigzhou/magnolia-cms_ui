1fa7e888ade79891754fa30939c506561f82db96
ehechinger <eric.hechinger@magnolia-cms.com>
2013-05-15 15:08:23 +0200

    MGNLUI-1277 : use Path.getValidatedLabel() to replace all invalid characters including white spaces.

--- magnolia-ui-vaadin-integration/src/main/java/info/magnolia/ui/vaadin/integration/jcr/AbstractJcrNodeAdapter.java
+++ magnolia-ui-vaadin-integration/src/main/java/info/magnolia/ui/vaadin/integration/jcr/AbstractJcrNodeAdapter.java
@@ -265,6 +265,7 @@ public abstract class AbstractJcrNodeAdapter extends AbstractJcrAdapter implemen
 
                     // make sure new path is clear
                     jcrName = Path.getUniqueLabel(node.getSession(), node.getParent().getPath(), jcrName);
+                    jcrName = Path.getValidatedLabel(jcrName);
                     String newPath = NodeUtil.combinePathAndName(node.getParent().getPath(), jcrName);
                     node.getSession().move(node.getPath(), newPath);
                     setPath(node.getPath());
