4320ca4efe3300df95bb11d03fde4550ec8674c6
Aleksandr Pchelintcev <aleksandr@vaadin.com>
2013-06-12 18:31:04 +0200

    MGNLUI-1645 Wrapped dialog buttons into TouchDelegate's in order to react on touch end events.

--- magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/gwt/client/dialog/widget/BaseDialogViewImpl.java
+++ magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/gwt/client/dialog/widget/BaseDialogViewImpl.java
@@ -45,6 +45,9 @@ import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ComplexPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.googlecode.mgwt.dom.client.event.touch.TouchEndEvent;
+import com.googlecode.mgwt.dom.client.event.touch.TouchEndHandler;
+import com.googlecode.mgwt.ui.client.widget.touch.TouchDelegate;
 
 /**
  * Basic implementation for {@link BaseDialogView}.
@@ -180,9 +183,10 @@ public class BaseDialogViewImpl extends ComplexPanel implements BaseDialogView {
             if (actionId.equalsIgnoreCase(defaultActionName)) {
                 button.addStyleDependentName("default");
             }
-            button.addClickHandler(new ClickHandler() {
+            TouchDelegate td = new TouchDelegate(button);
+            td.addTouchEndHandler(new TouchEndHandler() {
                 @Override
-                public void onClick(final ClickEvent event) {
+                public void onTouchEnd(TouchEndEvent touchEndEvent) {
                     getPresenter().fireAction(actionId);
                 }
             });
