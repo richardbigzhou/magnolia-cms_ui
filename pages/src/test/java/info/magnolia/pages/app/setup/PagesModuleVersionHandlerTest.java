ad87f3358137fd8132ebed25a1eb52dc32190400
ehechinger <eric.hechinger@magnolia-cms.com>
2013-07-05 08:22:09 +0200

    MGNLUI-1810 : Add a test class for the version handler.

---
+++ pages/src/test/java/info/magnolia/pages/app/setup/PagesModuleVersionHandlerTest.java
@@ -0,0 +1,115 @@
+/**
+ * This file Copyright (c) 2013 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.pages.app.setup;
+
+import static org.junit.Assert.*;
+
+import info.magnolia.context.MgnlContext;
+import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.module.ModuleManagementException;
+import info.magnolia.module.ModuleVersionHandler;
+import info.magnolia.module.ModuleVersionHandlerTestCase;
+import info.magnolia.module.model.Version;
+import info.magnolia.pages.setup.PagesModuleVersionHandler;
+import info.magnolia.repository.RepositoryConstants;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class.
+ */
+public class PagesModuleVersionHandlerTest extends ModuleVersionHandlerTestCase {
+
+    private Node dialog;
+
+    @Override
+    protected String getModuleDescriptorPath() {
+        return "/META-INF/magnolia/ui-admincentral.xml";
+    }
+
+    @Override
+    protected List<String> getModuleDescriptorPathsForTests() {
+        return Arrays.asList(
+                "/META-INF/magnolia/core.xml"
+                );
+    }
+
+    @Override
+    protected ModuleVersionHandler newModuleVersionHandlerForTests() {
+        return new PagesModuleVersionHandler();
+    }
+
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        Session session = MgnlContext.getJCRSession(RepositoryConstants.CONFIG);
+        dialog = NodeUtil.createPath(session.getRootNode(), "/modules/pages/dialogs", NodeTypes.ContentNode.NAME);
+        dialog.getSession().save();
+    }
+
+    @Test
+    public void testUpdateTo5_0_1WithoutExistingLinkDefinition() throws ModuleManagementException, RepositoryException {
+        // GIVEN
+        dialog.addNode("link", NodeTypes.ContentNode.NAME);
+        assertTrue(dialog.hasNode("link"));
+        // WHEN
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("5.0"));
+
+        // THEN
+        assertFalse(dialog.hasNode("link"));
+
+    }
+
+    @Test
+    public void testUpdateTo5_0_1WithoutNonExistingLinkDefinition() throws ModuleManagementException, RepositoryException {
+        // GIVEN
+
+        // WHEN
+        executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("5.0"));
+
+        // THEN
+        assertFalse(dialog.hasNode("link"));
+    }
+}
