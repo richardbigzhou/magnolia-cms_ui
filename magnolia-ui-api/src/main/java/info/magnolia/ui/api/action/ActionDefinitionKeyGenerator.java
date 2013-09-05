526f9b763122f7f755a8353c375087d68296e660
fgrilli <federico.grilli@magnolia-cms.com>
2013-09-05 13:19:10 +0200

    MGNLUI-2037 handle parent other than AppDescriptor - add a bunch of tests including default case of root being AppDescriptor

--- magnolia-ui-api/src/main/java/info/magnolia/ui/api/action/ActionDefinitionKeyGenerator.java
+++ magnolia-ui-api/src/main/java/info/magnolia/ui/api/action/ActionDefinitionKeyGenerator.java
@@ -34,13 +34,25 @@
 package info.magnolia.ui.api.action;
 
 import info.magnolia.i18n.AbstractI18nKeyGenerator;
+import info.magnolia.i18n.I18nKeyGenerator;
+import info.magnolia.i18n.NullKeyGenerator;
 import info.magnolia.ui.api.app.AppDescriptor;
 import info.magnolia.ui.api.app.SubAppDescriptor;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.reflections.ReflectionUtils;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 
 /**
  * An I18n key generator for the actionbar actions (labels, descriptions, errors, etc.).
@@ -58,24 +70,84 @@ public class ActionDefinitionKeyGenerator extends AbstractI18nKeyGenerator<Actio
     @Override
     protected void keysFor(List<String> keys, ActionDefinition actionDefinition, AnnotatedElement el) {
         final Object root = getRoot(actionDefinition);
+
         if (root instanceof AppDescriptor) {
             final AppDescriptor appDescriptor = (AppDescriptor) root;
             final SubAppDescriptor subAppDescriptor = getParentViaCast(actionDefinition);
             addKey(keys, appDescriptor.getName(), subAppDescriptor.getName(), "actions", actionDefinition.getName(), fieldOrGetterName(el));
         } else {
-            // for actions belonging to a MessageView
-            try {
-                final Method getId = root.getClass().getMethod("getId");
-                String messageViewId = (String) getId.invoke(root);
-                addKey(keys, messageViewId, "actions", actionDefinition.getName(), fieldOrGetterName(el));
-            } catch (NoSuchMethodException e) {
-                // not a MessageView - log this?
-            } catch (InvocationTargetException e) {
-                throw new RuntimeException(e); // TODO MGNLUI-2031
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e); // TODO MGNLUI-2031
+            final List<String> ancestorKeys = getKeysfromAncestors(actionDefinition, el, root);
+            if (ancestorKeys.isEmpty()) {
+                String idOrName = getIdOrNameForUnknownRoot(root);
+                if (idOrName == null) {
+                    addKey(keys, "actions", actionDefinition.getName(), fieldOrGetterName(el));
+                } else {
+                    addKey(keys, idOrName, "actions", actionDefinition.getName(), fieldOrGetterName(el));
+                }
+            } else {
+                addKey(keys, StringUtils.join(ancestorKeys, '.'), "actions", actionDefinition.getName(), fieldOrGetterName(el));
+            }
+        }
+    }
+
+    private List<String> getKeysfromAncestors(final ActionDefinition actionDefinition, final AnnotatedElement el, final Object root) {
+        final List<I18nKeyGenerator> keyGenerators = getAncestorKeyGenerators(actionDefinition);
+
+        final List<String> ancestorKeys = new ArrayList<String>();
+
+        for (I18nKeyGenerator keygen : keyGenerators) {
+            if (keygen instanceof NullKeyGenerator) {
+                continue;
             }
 
+            final String[] keysTmp = keygen.keysFor(null, root, el);
+            Collection<String> ancestorGeneratedKeys = Collections2.filter(Arrays.asList(keysTmp), new Predicate<String>() {
+
+                @Override
+                public boolean apply(String input) {
+                    if (StringUtils.isNotBlank(input) && !input.endsWith(".label")) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            if (ancestorGeneratedKeys.isEmpty()) {
+                continue;
+            }
+            final String key = ancestorGeneratedKeys.iterator().next();
+            ancestorKeys.add(key);
         }
+        return ancestorKeys;
+    }
+
+    private String getIdOrNameForUnknownRoot(Object root) {
+        @SuppressWarnings("unchecked")
+        final Set<Method> methods = ReflectionUtils.getMethods(root.getClass(), new Predicate<Method>() {
+
+            @Override
+            public boolean apply(Method input) {
+                if ("getId".equals(input.getName()) || "getName".equals(input.getName())) {
+                    return true;
+                }
+                return false;
+            }
+        });
+        try {
+            if (methods.isEmpty()) {
+                return null;
+            }
+            String idOrName = (String) methods.iterator().next().invoke(root);
+            return idOrName.replaceAll(":", ".");
+        } catch (IllegalArgumentException e) {
+            // TODO how do we handle this?
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO how do we handle this?
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO how do we handle this?
+            e.printStackTrace();
+        }
+        return null;
     }
 }
