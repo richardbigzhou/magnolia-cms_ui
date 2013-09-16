2202cc87d7bf26d672be70270a715dc2e90705d2
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2013-09-16 12:00:59 +0200

    MGNLUI-2103 Removed unnecessary start-up message.

--- magnolia-ui-dialog/src/main/java/info/magnolia/ui/dialog/registry/ConfiguredDialogDefinitionManager.java
+++ magnolia-ui-dialog/src/main/java/info/magnolia/ui/dialog/registry/ConfiguredDialogDefinitionManager.java
@@ -91,8 +91,6 @@ public class ConfiguredDialogDefinitionManager extends ModuleConfigurationObserv
                             if (provider != null) {
                                 providers.add(provider);
                             }
-                        } else {
-                            log.warn("node " + dialogNode.getName() + " will not be handled as Dialog.");
                         }
                     }
                 }
