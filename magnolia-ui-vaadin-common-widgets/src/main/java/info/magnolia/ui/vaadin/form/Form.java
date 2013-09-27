c24924586b79d1aaa15cc98509babc4f90c40709
Christopher Zimmermann <christopher.zimmermann@magnolia-cms.com>
2013-09-27 15:11:23 +0200

    MGNLUI-2146 REPLACING WITH SIMPLETRANSLATOR

--- magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/form/Form.java
+++ magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/form/Form.java
@@ -33,7 +33,6 @@
  */
 package info.magnolia.ui.vaadin.form;
 
-import info.magnolia.cms.i18n.MessagesUtil;
 import info.magnolia.ui.vaadin.form.tab.MagnoliaFormTab;
 import info.magnolia.ui.vaadin.gwt.client.form.connector.FormState;
 import info.magnolia.ui.vaadin.gwt.client.form.rpc.FormServerRpc;
@@ -58,7 +57,7 @@ import com.vaadin.ui.Field;
  */
 public class Form extends AbstractSingleComponentContainer implements FormViewReduced {
 
-    private final String SHOW_ALL = MessagesUtil.get("dialogs.show.all","info.magnolia.ui.admincentral.messages");
+    private final String SHOW_ALL = "Show All"; //TODO-TRANSLATE ("dialogs.show.all");
 
     private List<Field<?>> fields = new LinkedList<Field<?>>();
 
