87349a29a119cbaec5e1ea204016c4e72260a018
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2013-10-01 13:18:35 +0300

    MGNLUI-2175 Added a test case.

--- magnolia-ui-admincentral/src/test/java/info/magnolia/ui/admincentral/shellapp/favorites/FavoritesPresenterTest.java
+++ magnolia-ui-admincentral/src/test/java/info/magnolia/ui/admincentral/shellapp/favorites/FavoritesPresenterTest.java
@@ -33,9 +33,9 @@
  */
 package info.magnolia.ui.admincentral.shellapp.favorites;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
-import static org.hamcrest.CoreMatchers.*;
 
 import info.magnolia.cms.security.MgnlUserManager;
 import info.magnolia.cms.security.Realm;
@@ -44,20 +44,35 @@ import info.magnolia.cms.security.SecuritySupportImpl;
 import info.magnolia.cms.security.User;
 import info.magnolia.context.MgnlContext;
 import info.magnolia.context.SystemContext;
+import info.magnolia.i18nsystem.ContextLocaleProvider;
 import info.magnolia.i18nsystem.I18nizer;
+import info.magnolia.i18nsystem.LocaleProvider;
+import info.magnolia.i18nsystem.TranslationService;
+import info.magnolia.i18nsystem.proxytoys.ProxytoysI18nizer;
+import info.magnolia.registry.RegistrationException;
 import info.magnolia.test.ComponentsTestUtil;
 import info.magnolia.test.mock.MockWebContext;
 import info.magnolia.test.mock.jcr.MockSession;
 import info.magnolia.ui.api.app.registry.AppDescriptorRegistry;
+import info.magnolia.ui.api.app.registry.ConfiguredAppDescriptor;
 import info.magnolia.ui.framework.favorite.FavoriteStore;
+import info.magnolia.ui.vaadin.integration.jcr.JcrNodeAdapter;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Locale;
 
+import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.UI;
 
 /**
  * Tests.
@@ -78,8 +93,15 @@ public class FavoritesPresenterTest {
     private MockWebContext ctx;
     private FavoritesPresenter presenter;
 
+    private I18nizer i18nizer = new ProxytoysI18nizer(new TestTranslationService(), new ContextLocaleProvider() {
+        @Override
+        public Locale getLocale() {
+            return Locale.ENGLISH;
+        }
+    });
+
     @Before
-    public void setUp() {
+    public void setUp() throws RegistrationException {
         session = new MockSession(FavoriteStore.WORKSPACE_NAME);
         ctx = new MockWebContext();
         final User user = mock(User.class);
@@ -113,12 +135,36 @@ public class FavoritesPresenterTest {
         MgnlContext.setInstance(ctx);
 
         AppDescriptorRegistry registry = mock(AppDescriptorRegistry.class);
-        presenter = new FavoritesPresenter(null, null,registry, mock(I18nizer.class));
+
+        /**
+         * We mock a sample descriptor that would be returned when favorites presenter will ask.
+         * We do not set the name/title for sake of testing the i18n functionality.
+         */
+        ConfiguredAppDescriptor descriptor = new ConfiguredAppDescriptor();
+        doReturn(descriptor).when(registry).getAppDescriptor(anyString());
+
+        presenter = new FavoritesPresenter(null, new FavoritesManagerImpl(new FavoriteStore()),registry, i18nizer);
+        initializeVaadinUI();
     }
 
     @After
     public void tearDown() {
         MgnlContext.setInstance(null);
+    }
+
+    /**
+     * Test that when we determine favorite location from a previous fragment,
+     * we do not put the "null" string in the beginning (see http://jira.magnolia-cms.com/browse/MGNLUI-2175).
+     * @throws RepositoryException
+     */
+    @Test
+    public void checkDeterminePreviousLocationDoesNotContainNull() throws RepositoryException {
+        //WHEN
+        JcrNodeAdapter node = presenter.determinePreviousLocation();
+        node.applyChanges();
+
+        //THEN
+        assert(!node.getItemProperty("title").getValue().toString().startsWith("null"));
     }
 
     @Test
@@ -166,5 +212,52 @@ public class FavoritesPresenterTest {
 
         // THEN
         assertThat(result, equalTo(FRAGMENT));
+    }
+
+
+    private void initializeVaadinUI() {
+        UI.setCurrent(new UI() {
+            @Override
+            protected void init(VaadinRequest request) {
+            }
+
+            @Override
+            public Locale getLocale() {
+                return Locale.ENGLISH;
+            }
+
+            @Override
+            public Page getPage() {
+                Page page = mock(Page.class);
+                try {
+                    doReturn(new URI("http://test:8080/.magnolia/admincentral#app:test:test;")).when(page).getLocation();
+                } catch (URISyntaxException e) {
+                }
+                return page;
+            }
+        });
+
+        VaadinSession session = mock(VaadinSession.class);
+        when(session.hasLock()).thenReturn(true);
+        UI.getCurrent().setSession(session);
+        UI.getCurrent().getSession().lock();
+
+    }
+
+    /**
+     * Test translation service.
+     */
+    public static class TestTranslationService implements TranslationService {
+
+        @Override
+        public String translate(LocaleProvider localeProvider, String basename, String[] keys) {
+            return "translated with key [" + keys[0] + "] and basename [" + basename + "] and locale [" + localeProvider.getLocale() + "]";
+        }
+
+        @Override
+        public String translate(LocaleProvider localeProvider, String[] keys) {
+            return "translated with key [" + keys[0] + "] and locale [" + localeProvider.getLocale() + "]";
+        }
+
     }
 }
