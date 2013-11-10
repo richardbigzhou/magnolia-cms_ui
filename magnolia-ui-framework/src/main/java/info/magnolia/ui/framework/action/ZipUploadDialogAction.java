59f47182ab82d35c4d116cc0e4fdbba3bb72e501
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2013-11-10 21:25:08 +0200

    MGNLUI-2370 Created generic command and action for importing zip archives into JCR repository, added dependency on apache-commons-compress (1.0) and added import dialog.
    MGNLUI-2370 Added command configuration for zip import.
    
    MGNLUI-2370 Resolved empty folder path problems when uploading zip archive.
    
    MGNLUI-2370 Actually using encoding setting in ImportZipCommand, made fields required in zip upload dialog.
    
    MGNLUI-2370 JavaDoc.

---
+++ magnolia-ui-framework/src/main/java/info/magnolia/ui/framework/action/ZipUploadDialogAction.java
@@ -0,0 +1,130 @@
+/**
+ * This file Copyright (c) 2013 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.framework.action;
+
+import info.magnolia.commands.CommandsManager;
+import info.magnolia.commands.chain.Command;
+import info.magnolia.ui.framework.command.ImportZipCommand;
+import info.magnolia.ui.api.action.AbstractAction;
+import info.magnolia.ui.api.action.ActionExecutionException;
+import info.magnolia.ui.form.EditorCallback;
+import info.magnolia.ui.form.EditorValidator;
+import info.magnolia.ui.vaadin.integration.jcr.DefaultProperty;
+import info.magnolia.ui.vaadin.integration.jcr.JcrNodeAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jcr.RepositoryException;
+
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.jackrabbit.value.BinaryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.data.Item;
+
+/**
+ * Triggers zip archive upload command.
+ * @param <T> definition class.
+ * @see info.magnolia.ui.framework.command.ImportZipCommand
+ */
+public class ZipUploadDialogAction<T extends ZipUploadActionDefinition> extends AbstractAction<T> {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private CommandsManager commandsManager;
+
+    private Map<String, Object> params;
+
+    private EditorValidator validator;
+
+    private EditorCallback callback;
+
+    private Item item;
+
+    public ZipUploadDialogAction(T definition, final CommandsManager commandsManager, final EditorValidator validator, final EditorCallback callback, Item item) {
+        super(definition);
+        this.commandsManager = commandsManager;
+        this.validator = validator;
+        this.callback = callback;
+        this.item = item;
+    }
+
+    @Override
+    public void execute() throws ActionExecutionException {
+        validator.showValidation(true);
+        if (validator.isValid()) {
+            final JcrNodeAdapter itemChanged = (JcrNodeAdapter) item;
+            JcrNodeAdapter importNode = (JcrNodeAdapter) itemChanged.getChild("import");
+            if (importNode != null) {
+                executeCommand(itemChanged);
+            }
+            callback.onSuccess(getDefinition().getName());
+        } else {
+            log.info("Validation error(s) occurred. No Import performed.");
+        }
+    }
+
+    private void executeCommand(JcrNodeAdapter itemChanged) throws ActionExecutionException {
+        final String commandName = getDefinition().getCommand();
+        final String catalog = getDefinition().getCatalog();
+        final Command command = this.commandsManager.getCommand(catalog, commandName);
+
+        if (command == null) {
+            throw new ActionExecutionException(String.format("Could not find command [%s] in any catalog", commandName));
+        }
+
+        long start = System.currentTimeMillis();
+        try {
+            // Set the parameter used by the command.
+            setParams(itemChanged);
+            log.debug("Executing command [{}] from catalog [{}] with the following parameters [{}]...", new Object[] { commandName, catalog, params });
+            commandsManager.executeCommand(command, params);
+            log.debug("Command executed successfully in {} ms ", System.currentTimeMillis() - start);
+        } catch (Exception e) {
+            log.debug("Command execution failed after {} ms ", System.currentTimeMillis() - start);
+            throw new ActionExecutionException(e);
+        }
+    }
+
+    private void setParams(JcrNodeAdapter itemChanged) throws RepositoryException {
+        params = new HashMap<String, Object>();
+        JcrNodeAdapter importXml = (JcrNodeAdapter) itemChanged.getChild("import");
+        params.put(ImportZipCommand.STREAM_PROPERTY, ((DefaultProperty<BinaryImpl>) importXml.getItemProperty(JcrConstants.JCR_DATA)).getValue().getStream());
+        params.put(ImportZipCommand.ENCODING_PROPERTY, itemChanged.getItemProperty(ImportZipCommand.ENCODING_PROPERTY).getValue());
+        params.put("repository", itemChanged.getWorkspace());
+        params.put("path", itemChanged.getJcrItem().getPath());
+    }
+}
---
+++ magnolia-ui-framework/src/main/java/info/magnolia/ui/framework/command/ImportZipCommand.java
@@ -0,0 +1,234 @@
+/**
+ * This file Copyright (c) 2013 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.framework.command;
+
+import info.magnolia.cms.beans.config.MIMEMapping;
+import info.magnolia.cms.core.Path;
+import info.magnolia.commands.impl.BaseRepositoryCommand;
+import info.magnolia.context.Context;
+import info.magnolia.i18nsystem.SimpleTranslator;
+import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.ui.form.field.upload.UploadReceiver;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.apache.commons.collections.EnumerationUtils;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipFile;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.vaadin.easyuploads.UploadField;
+
+/**
+ * Imports ZIP archive contents into JCR repository.
+ */
+public class ImportZipCommand extends BaseRepositoryCommand {
+
+    public static final String STREAM_PROPERTY = "inputStream";
+
+    public static final String ENCODING_PROPERTY = "encoding";
+
+    public static final String ZIP_TMP_FILE_PREFIX = "zipupload";
+
+    public static final String ZIP_TMP_FILE_SUFFIX = ".zip";
+
+    public static final String DEFAULT_MIME_TYPE = "application/octet-stream";
+
+    public static final String BACKSLASH_DUMMY = "________backslash________";
+
+    private SimpleTranslator translator;
+
+    private InputStream inputStream;
+
+    protected Context context;
+
+    private String encoding;
+
+    @Inject
+    public ImportZipCommand(SimpleTranslator translator) {
+        this.translator = translator;
+    }
+
+    @Override
+    public boolean execute(Context context) throws Exception {
+        this.context = context;
+        File tmpFile =  null;
+        FileOutputStream tmpStream = null;
+        try {
+            tmpFile = File.createTempFile(ZIP_TMP_FILE_PREFIX, ZIP_TMP_FILE_SUFFIX);
+            tmpStream = new FileOutputStream(tmpFile);
+            IOUtils.copy(inputStream, tmpStream);
+        } catch (IOException e) {
+            log.error("Failed to dump zip file to temp file: ", e);
+            throw e;
+        } finally {
+            IOUtils.closeQuietly(tmpStream);
+            IOUtils.closeQuietly(inputStream);
+        }
+
+        if (isValid(tmpFile)) {
+            ZipFile zip = new ZipFile(tmpFile, getEncoding());
+            // We use the ant-1.6.5 zip package to workaround encoding issues of the sun implementation (http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4244499)
+            // For some reason, entries are not in the opposite order as how they appear in most tools - reversing here.
+            // Note that java.util.zip does not show this behaviour, and ant-1.7.1 seems to enumerate entries in alphabetical or random order.
+            // Another alternative might be http://truezip.dev.java.net
+            final List zipEntries = EnumerationUtils.toList(zip.getEntries());
+            Collections.sort(zipEntries, new Comparator() {
+                @Override
+                public int compare(Object first, Object second) {
+                    Boolean isFirstDirectory = ((ZipArchiveEntry)first).isDirectory();
+                    Boolean isSecondDirectory = ((ZipArchiveEntry)second).isDirectory();
+                    return isFirstDirectory.compareTo(isSecondDirectory);
+                }
+            });
+
+            Collections.reverse(zipEntries);
+            final Iterator it = zipEntries.iterator();
+            while (it.hasNext()) {
+                ZipArchiveEntry entry = (ZipArchiveEntry) it.next();
+                processEntry(zip, entry);
+            }
+            context.getJCRSession(getRepository()).save();
+        }
+        return false;
+    }
+
+    private void processEntry(ZipFile zip, ZipArchiveEntry entry) throws IOException, RepositoryException {
+            if (entry.getName().startsWith("__MACOSX")) {
+                return;
+            }
+            else if (entry.getName().endsWith(".DS_Store")) {
+                return;
+            }
+
+            if (entry.isDirectory()) {
+                ensureFolder(entry);
+            } else {
+                handleFileEntry(zip, entry);
+            }
+
+    }
+
+    protected void handleFileEntry(ZipFile zip, ZipArchiveEntry entry) throws IOException, RepositoryException {
+        String fileName = entry.getName();
+        if (StringUtils.contains(fileName, "/")) {
+            fileName = StringUtils.substringAfterLast(fileName, "/");
+        }
+
+        String extension = StringUtils.substringAfterLast(fileName, ".");
+        InputStream stream = zip.getInputStream(entry);
+        FileOutputStream os = null;
+        try {
+            UploadReceiver receiver = new UploadReceiver(Path.getTempDirectory(), translator);
+
+            String folderPath = extractEntryPath(entry);
+            if (folderPath.startsWith("/")) {
+                folderPath = folderPath.substring(1);
+            }
+            Node folder = StringUtils.isBlank(folderPath) ? getJCRNode(context) : getJCRNode(context).getNode(folderPath);
+            receiver.setFieldType(UploadField.FieldType.BYTE_ARRAY);
+            receiver.receiveUpload(fileName, StringUtils.defaultIfEmpty(MIMEMapping.getMIMEType(extension), DEFAULT_MIME_TYPE));
+            receiver.setValue(IOUtils.toByteArray(stream));
+
+            doHandleEntryFromReceiver(folder, receiver);
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            IOUtils.closeQuietly(stream);
+            IOUtils.closeQuietly(os);
+        }
+    }
+
+    /**
+     * Actually produce an asset or a node based on the entry data aggregated into an
+     * {@link UploadReceiver} object.
+     * @param folder parent folder node for a new entry.
+     * @param receiver wraps the entry file and its basic meta-information.
+     * @throws RepositoryException
+     */
+    protected void doHandleEntryFromReceiver(Node folder, UploadReceiver receiver) throws RepositoryException {}
+
+    private void ensureFolder(ZipArchiveEntry entry) throws RepositoryException {
+        String path = extractEntryPath(entry);
+        NodeUtil.createPath(getJCRNode(context), path, NodeTypes.Folder.NAME, true);
+    }
+
+    private String extractEntryPath(ZipArchiveEntry entry) {
+        String entryName = entry.getName();
+        String path = (StringUtils.contains(entryName, "/")) ?  StringUtils.substringBeforeLast(entryName, "/") : "/";
+
+        if (!path.startsWith("/")) {
+            path = "/" + path;
+        }
+
+        // make proper name, the path was already created
+        path = StringUtils.replace(path, "/", BACKSLASH_DUMMY);
+        path = Path.getValidatedLabel(path);
+        path = StringUtils.replace(path, BACKSLASH_DUMMY, "/");
+        return path;
+    }
+
+    private boolean isValid(File tmpFile) {
+        return tmpFile != null;
+    }
+
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    public void setInputStream(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    public String getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+}
