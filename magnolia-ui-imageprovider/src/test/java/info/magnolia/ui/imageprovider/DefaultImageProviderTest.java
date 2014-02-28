b9de654fbef60cf5525135ee250695166059ef1a
Jan Haderka <jan.haderka@magnolia-cms.com>
2014-02-28 16:02:52 +0100

    Update Copyright to 2014

--- magnolia-ui-imageprovider/src/test/java/info/magnolia/ui/imageprovider/DefaultImageProviderTest.java
+++ magnolia-ui-imageprovider/src/test/java/info/magnolia/ui/imageprovider/DefaultImageProviderTest.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2003-2013 Magnolia International
+ * This file Copyright (c) 2003-2014 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
