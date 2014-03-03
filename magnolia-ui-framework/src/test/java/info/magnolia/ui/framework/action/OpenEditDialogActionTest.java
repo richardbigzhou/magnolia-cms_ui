813981f66268f7c93bf0f2317844ffebf0ef5ab1
Milan Divilek <milan.divilek@magnolia-cms.com>
2014-03-03 14:21:57 -0500

    MGNLUI-2723 notification error message when dialog is not defined or registered

---
+++ magnolia-ui-framework/src/test/java/info/magnolia/ui/framework/action/OpenEditDialogActionTest.java
@@ -0,0 +1,96 @@
+/**
+ * This file Copyright (c) 2013-2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.framework.action;
+
+import static org.mockito.Mockito.*;
+
+import info.magnolia.event.EventBus;
+import info.magnolia.i18nsystem.SimpleTranslator;
+import info.magnolia.ui.api.action.ActionExecutionException;
+import info.magnolia.ui.api.context.UiContext;
+import info.magnolia.ui.dialog.formdialog.FormDialogPresenterFactory;
+import info.magnolia.ui.vaadin.overlay.MessageStyleTypeEnum;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OpenEditDialogActionTest {
+
+    private FormDialogPresenterFactory formDialogPresenterFactory;
+    private UiContext uiContext;
+    private SimpleTranslator i18n;
+
+    @Before
+    public void setUp() throws Exception {
+        formDialogPresenterFactory = mock(FormDialogPresenterFactory.class);
+        uiContext = mock(UiContext.class);
+        i18n = mock(SimpleTranslator.class);
+    }
+
+    @Test
+    public void testMissingDialogDefinition() throws ActionExecutionException {
+        // GIVEN
+        OpenEditDialogActionDefinition actionDefinition = new OpenEditDialogActionDefinition();
+        actionDefinition.setName("testAction");
+
+        when(i18n.translate("ui-framework.actions.no.dialog.definition", "testAction")).thenReturn("No dialog defined for action: testAction");
+
+        OpenEditDialogAction action = new OpenEditDialogAction(actionDefinition, null, formDialogPresenterFactory, uiContext, mock(EventBus.class), i18n);
+
+        // WHEN
+        action.execute();
+
+        // THEN
+        verify(uiContext).openNotification(MessageStyleTypeEnum.ERROR, false, "No dialog defined for action: testAction");
+    }
+
+    @Test
+    public void testDialogIsNotRegistered() throws ActionExecutionException {
+        // GIVEN
+        OpenEditDialogActionDefinition actionDefinition = new OpenEditDialogActionDefinition();
+        actionDefinition.setName("testAction");
+        actionDefinition.setDialogName("testDialog");
+
+        when(i18n.translate("ui-framework.actions.dialog.not.registered", "testDialog")).thenReturn("Dialog [testDialog] is not registered.");
+        when(formDialogPresenterFactory.createFormDialogPresenter("testDialog")).thenReturn(null);
+
+        OpenEditDialogAction action = new OpenEditDialogAction(actionDefinition, null, formDialogPresenterFactory, uiContext, mock(EventBus.class), i18n);
+
+        // WHEN
+        action.execute();
+
+        // THEN
+        verify(uiContext).openNotification(MessageStyleTypeEnum.ERROR, false, "Dialog [testDialog] is not registered.");
+    }
+}
