27d9d269de044b960c95f8fac39554355941b801
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2014-03-13 09:03:45 +0100

    JCR-agnostic apps concept adaptation to the components/field factories: mostly switching to Object item ids instead of String uuids.

--- magnolia-ui-contentapp/src/main/java/info/magnolia/ui/contentapp/field/WorkbenchField.java
+++ magnolia-ui-contentapp/src/main/java/info/magnolia/ui/contentapp/field/WorkbenchField.java
@@ -35,8 +35,8 @@ package info.magnolia.ui.contentapp.field;
 
 import info.magnolia.event.EventBus;
 import info.magnolia.ui.imageprovider.definition.ImageProviderDefinition;
-import info.magnolia.ui.vaadin.integration.NullItem;
 import info.magnolia.ui.vaadin.integration.jcr.JcrItemAdapter;
+import info.magnolia.ui.vaadin.integration.jcr.JcrItemId;
 import info.magnolia.ui.vaadin.integration.jcr.JcrItemUtil;
 import info.magnolia.ui.vaadin.integration.jcr.JcrNodeAdapter;
 import info.magnolia.ui.vaadin.integration.jcr.JcrPropertyAdapter;
@@ -54,7 +54,6 @@ import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -102,15 +101,7 @@ public class WorkbenchField extends CustomField<Object> {
         workbenchEventbus.addHandler(SelectionChangedEvent.class, new SelectionChangedEvent.Handler() {
             @Override
             public void onSelectionChanged(SelectionChangedEvent event) {
-                try {
-                    JcrItemAdapter firstItem = event.getFirstItem();
-                    Item value = !"/".equals(firstItem.getJcrItem().getPath()) ? firstItem : new NullItem();
-                    setValue(value, false);
-                } catch (RepositoryException e) {
-                    log.error("Error occurred while selecting an item in workbench: ", e);
-                    setValue(new NullItem(), false);
-                }
-
+                setValue(event.getFirstItemId(), false);
             }
         });
 
@@ -145,16 +136,16 @@ public class WorkbenchField extends CustomField<Object> {
             if (value instanceof  Item) {
                 return (Item)value;
             }
-            if (StringUtils.isBlank((String)value)) {
+
+            if (!(value instanceof JcrItemId)) {
                 return null;
             }
+
             String potentialPath = String.valueOf(value);
             try {
-                String itemId = JcrItemUtil.getItemId(workbenchDefinition.getWorkspace(), potentialPath);
-                if (!StringUtils.isBlank(itemId) || !JcrItemUtil.itemExists(workbenchDefinition.getWorkspace(), potentialPath)) {
-                    potentialPath = itemId;
-                }
-                javax.jcr.Item jcrItem = JcrItemUtil.getJcrItem(workbenchDefinition.getWorkspace(), potentialPath);
+                String workspace = workbenchDefinition.getWorkspace();
+                JcrItemId itemId = JcrItemUtil.getItemId(workspace, potentialPath);
+                javax.jcr.Item jcrItem = JcrItemUtil.getJcrItem(workspace, itemId);
                 return jcrItem.isNode() ?
                     new JcrNodeAdapter((Node) jcrItem):
                     new JcrPropertyAdapter((Property) jcrItem);
--- magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/grid/MagnoliaTable.java
+++ magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/grid/MagnoliaTable.java
@@ -38,7 +38,9 @@ import java.util.Map;
 
 import org.apache.commons.lang.StringEscapeUtils;
 
+import com.vaadin.data.Container;
 import com.vaadin.data.Property;
+import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.ui.Table;
 
 /**
@@ -46,8 +48,13 @@ import com.vaadin.ui.Table;
  */
 public class MagnoliaTable extends Table {
 
-    public MagnoliaTable() {
+    public MagnoliaTable(Container dataSource) {
+        super("", dataSource);
         addStyleName("v-magnolia-table");
+    }
+
+    public MagnoliaTable() {
+        this(new IndexedContainer());
     }
 
     @Override
