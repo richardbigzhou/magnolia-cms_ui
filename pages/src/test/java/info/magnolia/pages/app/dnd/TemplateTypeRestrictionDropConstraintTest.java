4c9723a576d17c3c8d215ac951328fdaf0eb9d5d
ehechinger <eric.hechinger@magnolia-cms.com>
2014-03-21 09:42:43 +0100

    MGNLUI-2737 as templateAssignment.getAvailableTemplates check the availability on passed parent node, create a tmp child node to the target node, and send it to templateAssignement

---
+++ pages/src/test/java/info/magnolia/pages/app/dnd/TemplateTypeRestrictionDropConstraintTest.java
@@ -0,0 +1,122 @@
+/**
+ * This file Copyright (c) 2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.pages.app.dnd;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import info.magnolia.context.MgnlContext;
+import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.rendering.template.TemplateDefinition;
+import info.magnolia.rendering.template.assignment.TemplateDefinitionAssignment;
+import info.magnolia.rendering.template.configured.ConfiguredTemplateDefinition;
+import info.magnolia.repository.RepositoryConstants;
+import info.magnolia.test.mock.MockContext;
+import info.magnolia.test.mock.MockUtil;
+import info.magnolia.test.mock.jcr.MockSession;
+import info.magnolia.ui.vaadin.integration.jcr.JcrNodeAdapter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.jcr.Node;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Main test class for {@link TemplateTypeRestrictionDropConstraint}.
+ */
+public class TemplateTypeRestrictionDropConstraintTest {
+
+    private Node source;
+    private Node target;
+    private JcrNodeAdapter sourceItem;
+    private JcrNodeAdapter targetItem;
+
+    private TemplateDefinitionAssignment templateAssignment;
+    private TemplateDefinition sourceTemplateDefinition;
+    private Collection<TemplateDefinition> availableTemplates;
+
+    @Before
+    public void setUp() throws Exception {
+        MockUtil.initMockContext();
+        final MockSession session = new MockSession(RepositoryConstants.WEBSITE);
+        Node root = session.getRootNode();
+        source = root.addNode("source", NodeTypes.Page.NAME);
+        sourceItem = new JcrNodeAdapter(source);
+
+        target = root.addNode("target", NodeTypes.Page.NAME);
+        targetItem = new JcrNodeAdapter(target);
+
+        ((MockContext) MgnlContext.getInstance()).addSession(RepositoryConstants.WEBSITE, session);
+
+        sourceTemplateDefinition = new ConfiguredTemplateDefinition(null);
+        templateAssignment = mock(TemplateDefinitionAssignment.class);
+        availableTemplates = new ArrayList<TemplateDefinition>();
+        when(templateAssignment.getAssignedTemplateDefinition(source)).thenReturn(sourceTemplateDefinition);
+        when(templateAssignment.getAvailableTemplates(any(Node.class))).thenReturn(availableTemplates);
+    }
+
+    @After
+    public void tearDown() {
+        MgnlContext.setInstance(null);
+    }
+
+    @Test
+    public void allowedAsChild() {
+        // GIVEN
+        TemplateTypeRestrictionDropConstraint constraint = new TemplateTypeRestrictionDropConstraint(templateAssignment);
+        availableTemplates.add(sourceTemplateDefinition);
+
+        // WHEN
+        boolean res = constraint.allowedAsChild(sourceItem, targetItem);
+
+        // THEN
+        assertTrue(res);
+    }
+
+    @Test
+    public void notAllowedAsChild() {
+        // GIVEN
+        TemplateTypeRestrictionDropConstraint constraint = new TemplateTypeRestrictionDropConstraint(templateAssignment);
+
+        // WHEN
+        boolean res = constraint.allowedAsChild(sourceItem, targetItem);
+
+        // THEN
+        assertFalse(res);
+    }
+}
