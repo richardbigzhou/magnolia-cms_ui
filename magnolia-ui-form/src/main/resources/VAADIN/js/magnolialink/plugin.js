8ee6139615ec4344b067499276ff1ee281539439
Mikaël Geljic <mikael.geljic@magnolia-cms.com>
2014-04-30 12:14:11 +0200

    QA removed java header from js with outdated year

--- magnolia-ui-form/src/main/resources/VAADIN/js/magnolialink/plugin.js
+++ magnolia-ui-form/src/main/resources/VAADIN/js/magnolialink/plugin.js
@@ -1,38 +1,9 @@
-/**
- * This file Copyright (c) 2010-2012 Magnolia International Ltd.
- * (http://www.magnolia-cms.com). All rights reserved.
- * 
- * 
- * This file is dual-licensed under both the Magnolia Network Agreement and the
- * GNU General Public License. You may elect to use one or the other of these
- * licenses.
- * 
- * This file is distributed in the hope that it will be useful, but AS-IS and
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT. Redistribution,
- * except as permitted by whichever of the GPL or MNA you select, is prohibited.
- * 
- * 1. For the GPL license (GPL), you can redistribute and/or modify this file
- * under the terms of the GNU General Public License, Version 3, as published by
- * the Free Software Foundation. You should have received a copy of the GNU
- * General Public License, Version 3 along with this program; if not, write to
- * the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA.
- * 
- * 2. For the Magnolia Network Agreement (MNA), this file and the accompanying
- * materials are made available under the terms of the MNA which accompanies
- * this distribution, and is available at http://www.magnolia-cms.com/mna.html
- * 
- * Any modifications to this file must keep this entire header intact.
- * 
- */
-
 (function() {
     var EVENT_SEND_MAGNOLIA_LINK = "mgnlLinkSelected";
     var EVENT_CANCEL_LINK = "mgnlLinkCancel";
     var EVENT_GET_MAGNOLIA_LINK = "mgnlGetLink";
     var WORKSPACES = ['website', 'dam'];
-    
+
     CKEDITOR.plugins.add('magnolialink', {
         init: function(editor) {
             editor.ui.addButton('InternalLink', {
@@ -40,7 +11,7 @@
                 command: 'website',
                 icon: "../../../themes/admincentraltheme/img/s02-internal-link.png"
             });
-            
+
             editor.ui.addButton('DamLink', {
                 label: 'Link to DAM document',
                 command: 'dam',
@@ -49,14 +20,14 @@
 
             editor.addMenuGroup('mlinkgroup');
 
-            editor.addMenuItem('website', { 
+            editor.addMenuItem('website', {
                 label: 'Edit Magnolia Link',
                 command: 'website',
                 group: 'mlinkgroup',
                 icon: "../../../themes/admincentraltheme/img/s02-internal-link.png"
             });
-            
-            editor.addMenuItem('dam', { 
+
+            editor.addMenuItem('dam', {
                 label: 'Edit DAM Link',
                 command: 'dam',
                 group: 'mlinkgroup',
@@ -68,14 +39,14 @@
              * is needed to restore selection after dialog has closed.
              */
             var selectionRangeHack = null;
-            
+
             function reqDialog(workspace) {
                 return function(editor) {
                     selectionRangeHack = editor.getSelection().getRanges(true);
                     var selectedElement = CKEDITOR.plugins.link.getSelectedLink(editor);
-                    
+
                     if (isInternalLink(selectedElement)) {
-                        var href = selectedElement.getAttribute('href');                        
+                        var href = selectedElement.getAttribute('href');
                         var path = href.match(/path\:\{([^\}]*)\}/);
                         var repository = getWorkspace(selectedElement);
 
@@ -83,7 +54,7 @@
                             path = href.match(/handle\:\{([^\}]*)\}/);
                         }
 
-                        editor.fire(EVENT_GET_MAGNOLIA_LINK, '{\'workspace\' :\''+ repository+'\', \'path\': \''+path[1]+'\'}');                      
+                        editor.fire(EVENT_GET_MAGNOLIA_LINK, '{\'workspace\' :\''+ repository+'\', \'path\': \''+path[1]+'\'}');
                     } else {
                         editor.fire(EVENT_GET_MAGNOLIA_LINK, '{\'workspace\' :\''+ workspace+'\'}');
                     }
@@ -102,7 +73,7 @@
             // Respond from Pages app
             editor.on(EVENT_SEND_MAGNOLIA_LINK, function(e) {
                 setReadOnly(editor, false);
-                editor.getSelection().selectRanges(selectionRangeHack);             
+                editor.getSelection().selectRanges(selectionRangeHack);
                 var selectedElement = CKEDITOR.plugins.link.getSelectedLink(editor);
                 var link = eval('('+e.data+')');
                 var href = '${link:{uuid:{'+link.identifier+
@@ -131,14 +102,14 @@
                 if (e.data) {
                     alert(e.data);
                 }
-                
+
                 setReadOnly(editor, false);
             });
 
             // Double click
-            editor.on('doubleclick', function(ev) {             
+            editor.on('doubleclick', function(ev) {
                 var selected = CKEDITOR.plugins.link.getSelectedLink(editor);
-                
+
                 if (isInternalLink(selected)) {
                     ev.data.dialog = null; // Eat the CK link dialog
                     editor.getCommand(getWorkspace(selected)).exec(editor);
@@ -152,36 +123,36 @@
                 }
 
                 var element = evt.data.path.lastElement && evt.data.path.lastElement.getAscendant( 'a', true );
-                
+
                 if (isLink(element)) {
                     for (var index = 0; index < WORKSPACES.length; index++) {
                         editor.getCommand(WORKSPACES[index]).setState(CKEDITOR.TRISTATE_DISABLED);
                     }
                     editor.getCommand('link').setState(CKEDITOR.TRISTATE_DISABLED);
                     editor.getCommand('unlink').setState(CKEDITOR.TRISTATE_OFF);
-                    
+
                     var workspace = getWorkspace(element);
                     if (workspace) {
                         editor.getCommand(workspace).setState(CKEDITOR.TRISTATE_OFF);
                     } else {
                         editor.getCommand('link').setState(CKEDITOR.TRISTATE_OFF);
                     }
-                                        
+
                 } else {
                     for (var index = 0; index < WORKSPACES.length; index++) {
-                        editor.getCommand(WORKSPACES[index]).setState(CKEDITOR.TRISTATE_OFF);                        
+                        editor.getCommand(WORKSPACES[index]).setState(CKEDITOR.TRISTATE_OFF);
                     }
-                    
+
                     editor.getCommand('link').setState(CKEDITOR.TRISTATE_OFF);
                     editor.getCommand('unlink').setState(CKEDITOR.TRISTATE_DISABLED);
                 }
             });
 
             // Context menu
-            editor.contextMenu.addListener(function(element, selection) {       
+            editor.contextMenu.addListener(function(element, selection) {
                 if (!isInternalLink(element)) {
                     return null;
-                } 
+                }
 
                 var retval = {};
                 retval[getWorkspace(element)] = CKEDITOR.TRISTATE_OFF;
@@ -189,13 +160,13 @@
             });
         }
     });
-    
+
     function getWorkspace(element) {
-        var href = element.getAttribute('href');                        
+        var href = element.getAttribute('href');
         var repository = href.match(/repository\:\{([^\}]*)\}/);
-        
+
         if (repository) {
-            return repository[1];            
+            return repository[1];
         } else {
             return false;
         }
