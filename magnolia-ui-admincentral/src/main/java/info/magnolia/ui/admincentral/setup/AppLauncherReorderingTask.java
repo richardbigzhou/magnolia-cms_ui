73161e4729b64ee22ab3434e01db3cbf353ad1d6
Mikaël Geljić <mikael.geljic@magnolia-cms.com>
2014-05-21 10:22:38 +0200

    MGNLUI-2843 add AppLauncherReorderingTask to ease reordering apps from other modules (one-liner, less error-prone)

---
+++ magnolia-ui-admincentral/src/main/java/info/magnolia/ui/admincentral/setup/AppLauncherReorderingTask.java
@@ -0,0 +1,117 @@
+/**
+ * This file Copyright (c) 2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.admincentral.setup;
+
+import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.AbstractRepositoryTask;
+import info.magnolia.module.delta.TaskExecutionException;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A basic safe reordering task to make it easy to rearrange order of apps on the applauncher.
+ */
+public class AppLauncherReorderingTask extends AbstractRepositoryTask {
+
+    private static final Logger log = LoggerFactory.getLogger(AppLauncherReorderingTask.class);
+
+    private final String appName;
+    private final String groupName;
+    private final Order order;
+    private final String relativeAppName;
+
+    public AppLauncherReorderingTask(String appName, String groupName, Order order, String relativeAppName) {
+        super("Reorder applauncher", String.format("This task reorders the %s app in the %s group of the app-launcher.", appName, groupName));
+
+        this.appName = appName;
+        this.groupName = groupName;
+        this.order = order;
+        this.relativeAppName = relativeAppName;
+    }
+
+    @Override
+    protected void doExecute(InstallContext ctx) throws RepositoryException, TaskExecutionException {
+
+        Session session = ctx.getConfigJCRSession();
+
+        if (!session.nodeExists(String.format("/modules/ui-admincentral/config/appLauncherLayout/groups/%s/apps/%s", groupName, appName))) {
+            log.warn("Not reordering the %s app in the %s group because either the group or the app doesn't exist.", appName, groupName);
+            return;
+        }
+
+        Node apps = session.getNode(String.format("/modules/ui-admincentral/config/appLauncherLayout/groups/%s/apps", groupName));
+        Node app = apps.getNode(appName);
+
+        switch (order) {
+        case FIRST:
+            NodeUtil.orderFirst(app);
+            break;
+
+        case BEFORE:
+            if (apps.hasNode(relativeAppName)) {
+                NodeUtil.orderBefore(app, relativeAppName);
+            } else {
+                log.warn("Not reordering the %s app in the %s group because the relative app %s does not exist.", appName, groupName, relativeAppName);
+            }
+            break;
+
+        case AFTER:
+            if (apps.hasNode(relativeAppName)) {
+                NodeUtil.orderAfter(app, relativeAppName);
+            } else {
+                log.warn("Not reordering the %s app in the %s group because the relative app %s does not exist.", appName, groupName, relativeAppName);
+            }
+            break;
+
+        case LAST:
+            NodeUtil.orderLast(app);
+            break;
+
+        }
+    }
+
+    /**
+     * The type of the applauncher reordering operation.
+     */
+    public static enum Order {
+        FIRST, BEFORE, AFTER, LAST
+    }
+
+}
---
+++ magnolia-ui-admincentral/src/test/java/info/magnolia/ui/admincentral/setup/AppLauncherReorderingTaskTest.java
@@ -0,0 +1,141 @@
+/**
+ * This file Copyright (c) 2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.admincentral.setup;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.module.InstallContext;
+import info.magnolia.repository.RepositoryConstants;
+import info.magnolia.test.mock.jcr.MockSession;
+import info.magnolia.ui.admincentral.setup.AppLauncherReorderingTask.Order;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Session;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The AppLauncherReorderingTaskTest.
+ */
+public class AppLauncherReorderingTaskTest {
+
+    private Session session;
+    private InstallContext ctx;
+
+    private Node apps;
+
+    @Before
+    public void setUp() throws Exception {
+        session = new MockSession(RepositoryConstants.CONFIG);
+        ctx = mock(InstallContext.class);
+        doReturn(session).when(ctx).getJCRSession(RepositoryConstants.CONFIG);
+        doReturn(session).when(ctx).getConfigJCRSession();
+
+        apps = NodeUtil.createPath(session.getRootNode(), "/modules/ui-admincentral/config/appLauncherLayout/groups/samples/apps", NodeTypes.ContentNode.NAME);
+        apps.addNode("foxtrot");
+        apps.addNode("uniform");
+        apps.addNode("charlie");
+        apps.addNode("kilo");
+    }
+
+    @Test
+    public void reorderAppAsFirstInGroup() throws Exception {
+        // GIVEN
+        AppLauncherReorderingTask task = new AppLauncherReorderingTask("charlie", "samples", Order.FIRST, null);
+
+        // WHEN
+        task.execute(ctx);
+
+        // THEN
+        NodeIterator it = apps.getNodes();
+        assertEquals("charlie", it.nextNode().getName());
+        assertEquals("foxtrot", it.nextNode().getName());
+        assertEquals("uniform", it.nextNode().getName());
+        assertEquals("kilo", it.nextNode().getName());
+    }
+
+    @Test
+    public void reorderAppBefore() throws Exception {
+        // GIVEN
+        AppLauncherReorderingTask task = new AppLauncherReorderingTask("charlie", "samples", Order.BEFORE, "uniform");
+
+        // WHEN
+        task.execute(ctx);
+
+        // THEN
+        NodeIterator it = apps.getNodes();
+        assertEquals("foxtrot", it.nextNode().getName());
+        assertEquals("charlie", it.nextNode().getName());
+        assertEquals("uniform", it.nextNode().getName());
+        assertEquals("kilo", it.nextNode().getName());
+    }
+
+    @Test
+    public void reorderAppAfter() throws Exception {
+        // GIVEN
+        AppLauncherReorderingTask task = new AppLauncherReorderingTask("foxtrot", "samples", Order.AFTER, "charlie");
+
+        // WHEN
+        task.execute(ctx);
+
+        // THEN
+        NodeIterator it = apps.getNodes();
+        assertEquals("uniform", it.nextNode().getName());
+        assertEquals("charlie", it.nextNode().getName());
+        assertEquals("foxtrot", it.nextNode().getName());
+        assertEquals("kilo", it.nextNode().getName());
+    }
+
+    @Test
+    public void reorderAppAsLastInGroup() throws Exception {
+        // GIVEN
+        AppLauncherReorderingTask task = new AppLauncherReorderingTask("uniform", "samples", Order.LAST, null);
+
+        // WHEN
+        task.execute(ctx);
+
+        // THEN
+        NodeIterator it = apps.getNodes();
+        assertEquals("foxtrot", it.nextNode().getName());
+        assertEquals("charlie", it.nextNode().getName());
+        assertEquals("kilo", it.nextNode().getName());
+        assertEquals("uniform", it.nextNode().getName());
+    }
+
+}
