15ff11e01cf1a87cdbb7bbc8fb0252f01c04de91
rkovarik <roman.kovarik@magnolia-cms.com>
2014-06-03 08:21:05 +0200

    MGNLUI-2961 implement action for opening a location

---
+++ magnolia-ui-framework/src/main/java/info/magnolia/ui/framework/action/OpenLocationAction.java
@@ -0,0 +1,69 @@
+/**
+ * This file Copyright (c) 2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.framework.action;
+
+import info.magnolia.ui.api.action.AbstractAction;
+import info.magnolia.ui.api.action.ActionExecutionException;
+import info.magnolia.ui.api.location.DefaultLocation;
+import info.magnolia.ui.api.location.Location;
+import info.magnolia.ui.api.location.LocationController;
+
+import javax.inject.Inject;
+
+/**
+ * The {@link OpenLocationAction} opens a given location in the admincentral.
+ * 
+ * @see {@link OpenLocationActionDefinition}
+ */
+public class OpenLocationAction extends AbstractAction<OpenLocationActionDefinition> {
+
+    protected final LocationController locationController;
+
+    @Inject
+    public OpenLocationAction(OpenLocationActionDefinition definition, LocationController locationController) {
+        super(definition);
+        this.locationController = locationController;
+    }
+
+    @Override
+    public void execute() throws ActionExecutionException {
+        final String appType = this.getDefinition().getAppType();
+        final String appName = this.getDefinition().getAppName();
+        final String subAppId = this.getDefinition().getSubAppId();
+        final String parameter = this.getDefinition().getParameter();
+
+        Location location = new DefaultLocation(appType, appName, subAppId, parameter);
+        locationController.goTo(location);
+    }
+}
---
+++ magnolia-ui-framework/src/main/java/info/magnolia/ui/framework/action/OpenLocationActionDefinition.java
@@ -0,0 +1,91 @@
+/**
+ * This file Copyright (c) 2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.framework.action;
+
+import info.magnolia.ui.api.action.ConfiguredActionDefinition;
+import info.magnolia.ui.api.location.Location;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Defines an action for opening any location in the admincentral.
+ * <p>
+ * This supports setting the location type, the app name, sub-app name as well as the parameter part of the location fragment, for the target app to use.
+ *
+ * @see {@link Location}
+ */
+public class OpenLocationActionDefinition extends ConfiguredActionDefinition {
+
+    private String appType = Location.LOCATION_TYPE_APP;
+    private String appName = StringUtils.EMPTY;
+    private String subAppId = StringUtils.EMPTY;
+    private String parameter = StringUtils.EMPTY;
+
+    public OpenLocationActionDefinition() {
+        setImplementationClass(OpenLocationAction.class);
+    }
+
+    public String getAppType() {
+        return appType;
+    }
+
+    public void setAppType(String appType) {
+        this.appType = appType;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getSubAppId() {
+        return subAppId;
+    }
+
+    public void setSubAppId(String subAppId) {
+        this.subAppId = subAppId;
+    }
+
+    public String getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(String parameter) {
+        this.parameter = parameter;
+    }
+
+}
