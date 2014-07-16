f797a05c302e33f18e06d54b4a3544d306f095b9
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2014-07-16 23:58:48 +0300

    MGNLUI-1800 Created stub app/sub-app for the case of the failure during start up. - Opened automatically during start-up. - Reset event handlers created for failed subapp. - Send failure message to pulse, show error bar.

---
+++ magnolia-ui-framework/src/main/java/info/magnolia/ui/framework/app/stub/ExceptionMessage.java
@@ -0,0 +1,75 @@
+/**
+ * This file Copyright (c) 2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.framework.app.stub;
+
+import info.magnolia.i18nsystem.SimpleTranslator;
+import info.magnolia.ui.api.message.Message;
+import info.magnolia.ui.api.message.MessageType;
+import info.magnolia.util.EscapeUtil;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+/**
+ * Convenience class capable of crating a {@link info.magnolia.ui.api.message.Message} based on the
+ * exception passed as a c-tor argument.
+ */
+class ExceptionMessage extends Message {
+
+    public static final String STACK_TRACE_KEY = "ui-framework.message.exception.stacktrace";
+    public static final String EXCEPTION_MSG_KEY = "ui-framework.message.exception.message";
+
+    private Throwable relatedException;
+
+    private SimpleTranslator i18n;
+
+    ExceptionMessage(Throwable relatedException, String subjectMessage, SimpleTranslator i18n) {
+        this.relatedException = relatedException;
+        this.i18n = i18n;
+        setType(MessageType.ERROR);
+        setSubject(subjectMessage);
+        setMessage(prepareMessage());
+    }
+
+    private String prepareMessage() {
+        final StringBuilder sb = new StringBuilder();
+        if (relatedException.getLocalizedMessage() != null) {
+            sb.append(i18n.translate(EXCEPTION_MSG_KEY)).append(" ").append(EscapeUtil.escapeXss(relatedException.getLocalizedMessage()));
+            sb.append(". ");
+        }
+        sb.append(i18n.translate(STACK_TRACE_KEY)).append(" ").append(ExceptionUtils.getStackTrace(relatedException));
+        return sb.toString();
+    }
+
+
+}
---
+++ magnolia-ui-framework/src/main/java/info/magnolia/ui/framework/app/stub/FailedAppStub.java
@@ -0,0 +1,82 @@
+/**
+ * This file Copyright (c) 2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.framework.app.stub;
+
+import info.magnolia.i18nsystem.SimpleTranslator;
+import info.magnolia.ui.api.app.AppContext;
+import info.magnolia.ui.api.location.Location;
+import info.magnolia.ui.api.message.Message;
+import info.magnolia.ui.framework.app.BaseApp;
+import info.magnolia.ui.framework.app.DefaultAppView;
+
+import javax.inject.Inject;
+
+/**
+ * Stub {@link info.magnolia.ui.framework.app.BaseApp} extension showed in case an exception
+ * was thrown during the original app start-up phase. Displays a minimal view and sends the
+ * exception details to the Pulse in a form of a message.
+ */
+public class FailedAppStub extends BaseApp {
+
+    public static final String MSG_KEY = "ui-framework.app.start.failed";
+
+    private Throwable relatedException;
+
+    private SimpleTranslator i18n;
+
+    @Inject
+    public FailedAppStub(final AppContext appContext, Throwable relatedException, SimpleTranslator i18n) {
+        super(appContext, new DefaultAppView());
+        this.relatedException = relatedException;
+        this.i18n = i18n;
+    }
+
+    @Override
+    public void locationChanged(Location location) {
+        // Do nothing
+    }
+
+    @Override
+    public void start(Location location) {
+        final String appName = appContext.getName();
+        final String message = i18n.translate(MSG_KEY, appName);
+
+        Message error = new ExceptionMessage(relatedException, message, i18n);
+        ((DefaultAppView)getView()).asVaadinComponent().addComponent(new StubView("icon-warning").asVaadinComponent());
+
+        appContext.sendLocalMessage(error);
+    }
+
+}
+
