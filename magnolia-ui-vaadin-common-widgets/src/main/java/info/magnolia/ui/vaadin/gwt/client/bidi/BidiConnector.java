b490dc1d59e68637f8adf30442802575588fac2d
Mikaël Geljić <mikael.geljic@magnolia-cms.com>
2014-08-18 15:41:47 +0200

    RTL initial Vaadin extension for adding bidi text support to basic input fields

---
+++ magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/gwt/client/bidi/BidiConnector.java
@@ -0,0 +1,80 @@
+/**
+ * This file Copyright (c) 2014 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.ui.vaadin.gwt.client.bidi;
+
+import info.magnolia.ui.vaadin.bidi.Bidi;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.vaadin.client.ComponentConnector;
+import com.vaadin.client.ServerConnector;
+import com.vaadin.client.communication.StateChangeEvent;
+import com.vaadin.client.extensions.AbstractExtensionConnector;
+import com.vaadin.shared.ui.Connect;
+
+/**
+ * Client-side connector for the {@link Bidi} extension.
+ */
+@Connect(Bidi.class)
+public class BidiConnector extends AbstractExtensionConnector {
+
+    private Widget textWidget;
+
+    @Override
+    public void onStateChanged(StateChangeEvent stateChangeEvent) {
+        super.onStateChanged(stateChangeEvent);
+        if (stateChangeEvent.hasPropertyChanged("bidi")) {
+            setBidi(getState().bidi);
+        }
+    }
+
+    @Override
+    public ComponentConnector getParent() {
+        return (ComponentConnector) super.getParent();
+    }
+
+    @Override
+    protected void extend(ServerConnector target) {
+        this.textWidget = ((ComponentConnector) target).getWidget();
+    }
+
+    private void setBidi(boolean bidi) {
+        this.textWidget.getElement().setAttribute("dir", "auto");
+    }
+
+    @Override
+    public BidiState getState() {
+        return (BidiState) super.getState();
+    }
+
+}
