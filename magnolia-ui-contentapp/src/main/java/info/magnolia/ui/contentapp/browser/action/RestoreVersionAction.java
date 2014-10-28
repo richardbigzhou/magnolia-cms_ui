24c682fa9edf6e48acc490df9cdb1c4b4d1eeae1
Philip Mundt <philip.mundt@magnolia-cms.com>
2014-10-28 08:48:29 +0100

    MGNLUI-3220 Ask whether to restore the last version (when version store is full) without creating a new version of a page; Add tests

--- magnolia-ui-contentapp/src/main/java/info/magnolia/ui/contentapp/browser/action/RestoreVersionAction.java
+++ magnolia-ui-contentapp/src/main/java/info/magnolia/ui/contentapp/browser/action/RestoreVersionAction.java
@@ -33,15 +33,19 @@
  */
 package info.magnolia.ui.contentapp.browser.action;
 
+import info.magnolia.cms.beans.config.VersionConfig;
 import info.magnolia.cms.core.version.VersionManager;
 import info.magnolia.event.EventBus;
 import info.magnolia.i18nsystem.SimpleTranslator;
 import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.jcr.util.VersionUtil;
+import info.magnolia.objectfactory.Components;
 import info.magnolia.ui.api.app.AppContext;
 import info.magnolia.ui.api.context.UiContext;
 import info.magnolia.ui.api.event.AdmincentralEventBus;
 import info.magnolia.ui.api.event.ContentChangedEvent;
 import info.magnolia.ui.api.location.LocationController;
+import info.magnolia.ui.api.overlay.ConfirmationCallback;
 import info.magnolia.ui.dialog.formdialog.FormDialogPresenter;
 import info.magnolia.ui.form.EditorCallback;
 import info.magnolia.ui.vaadin.integration.jcr.AbstractJcrNodeAdapter;
@@ -52,6 +56,7 @@ import javax.inject.Named;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.version.Version;
+import javax.jcr.version.VersionHistory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,11 +72,22 @@ public class RestoreVersionAction extends ShowVersionsAction<RestoreVersionActio
 
     private final EventBus eventBus;
 
-    @Inject
+    private final VersionConfig versionConfig;
+
+    /**
+     * @deprecated since 5.3.5 - use {@link RestoreVersionAction(RestoreVersionActionDefinition, AppContext, LocationController, UiContext, FormDialogPresenter, AbstractJcrNodeAdapter, SimpleTranslator, VersionManager, EventBus, VersionConfig)} instead.
+     */
+    @Deprecated
     public RestoreVersionAction(RestoreVersionActionDefinition definition, AppContext appContext, LocationController locationController, UiContext uiContext, FormDialogPresenter formDialogPresenter, AbstractJcrNodeAdapter nodeAdapter, SimpleTranslator i18n, VersionManager versionManager, final @Named(AdmincentralEventBus.NAME) EventBus eventBus) {
+        this(definition, appContext, locationController, uiContext, formDialogPresenter, nodeAdapter, i18n, versionManager, eventBus, Components.getComponent(VersionConfig.class));
+    }
+
+    @Inject
+    public RestoreVersionAction(RestoreVersionActionDefinition definition, AppContext appContext, LocationController locationController, UiContext uiContext, FormDialogPresenter formDialogPresenter, AbstractJcrNodeAdapter nodeAdapter, SimpleTranslator i18n, VersionManager versionManager, final @Named(AdmincentralEventBus.NAME) EventBus eventBus, VersionConfig versionConfig) {
         super(definition, appContext, locationController, uiContext, formDialogPresenter, nodeAdapter, i18n);
         this.versionManager = versionManager;
         this.eventBus = eventBus;
+        this.versionConfig = versionConfig;
         this.dialogID = "ui-contentapp:code:RestoreVersionAction.selectVersion";
     }
 
@@ -82,21 +98,31 @@ public class RestoreVersionAction extends ShowVersionsAction<RestoreVersionActio
             public void onSuccess(String actionName) {
                 try {
                     Node node = getNode();
+                    String versionName = getVersionName();
+                    VersionHistory versionHistory = versionManager.getVersionHistory(node);
 
                     if (getDefinition().isCreateVersionBeforeRestore()) {
-                        createVersionBeforeRestore(node);
-                    }
+                        long versionsSize = versionHistory.getAllVersions().getSize() - 2; // Do not consider the root version
 
-                    String versionName = getVersionName();
-                    versionManager.restore(node, versionManager.getVersion(node, versionName), true);
-                    eventBus.fireEvent(new ContentChangedEvent(nodeAdapter.getItemId()));
-                    uiContext.openNotification(MessageStyleTypeEnum.INFO, true, i18n.translate("ui-contentapp.actions.restoreVersion.notification.success"));
+                        if (!VersionUtil.hasPreviousVersion(node, versionName) && versionsSize >= versionConfig.getMaxVersionAllowed()) {
+                            uiContext.openConfirmation(
+                                    MessageStyleTypeEnum.WARNING,
+                                    i18n.translate("ui-contentapp.actions.restoreVersion.confirmation.title"),
+                                    i18n.translate("ui-contentapp.actions.restoreVersion.confirmation.body"),
+                                    i18n.translate("ui-contentapp.actions.restoreVersion.confirmation.confirmButton"),
+                                    i18n.translate("ui-contentapp.actions.restoreVersion.confirmation.cancelButton"),
+                                    false,
+                                    getConfirmationCallback());
+                        } else {
+                            restoreVersion(node, versionName, true);
+                        }
+                    } else {
+                        restoreVersion(node, versionName, false);
+                    }
                 } catch (RepositoryException e) {
                     uiContext.openNotification(MessageStyleTypeEnum.ERROR, true, i18n.translate("ui-contentapp.actions.restoreVersion.notification.error", e.getMessage()));
-                    log.error(i18n.translate("ui-contentapp.actions.restoreVersion.notification.error", e.getMessage()));
+                    log.error(i18n.translate("ui-contentapp.actions.restoreVersion.notification.error", e.getMessage()), e);
                 }
-
-                formDialogPresenter.closeDialog();
             }
 
             @Override
@@ -106,6 +132,31 @@ public class RestoreVersionAction extends ShowVersionsAction<RestoreVersionActio
         };
     }
 
+    /**
+     * Confirmation callback when version store is full and user has to decide whether to restore the last version
+     * without creating another version of the page before restoring.
+     */
+    protected ConfirmationCallback getConfirmationCallback() {
+        return new ConfirmationCallback() {
+            @Override
+            public void onSuccess() {
+                try {
+                    restoreVersion(getNode(), getVersionName(), false);
+                } catch (RepositoryException e) {
+                    log.error(i18n.translate("ui-contentapp.actions.restoreVersion.notification.error", e.getMessage()), e);
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                // Just close the confirmation but do not close the dialog
+            }
+        };
+    }
+
+    /**
+     * Creates a version with an extra comment before restoring.
+     */
     protected Version createVersionBeforeRestore(Node node) throws RepositoryException {
         NodeTypes.Versionable.set(node, i18n.translate("ui-contentapp.actions.restoreVersion.comment.restore"));
         node.getSession().save();
@@ -113,6 +164,35 @@ public class RestoreVersionAction extends ShowVersionsAction<RestoreVersionActio
         NodeTypes.Versionable.set(node, null);
         node.getSession().save();
         return version;
+    }
+
+    /**
+     * Restores a version by its version name.
+     *
+     * By default {@link RestoreVersionActionDefinition#createVersionBeforeRestore} is true thus a version is created
+     * before restore. When {@link VersionConfig#maxVersions} is reached such a version cannot be created when restoring
+     * the oldest version (without deleting that same version). In this case {@link #getConfirmationCallback()} is shown
+     * and once confirmed the version gets restored without "backup" of the current content.
+     *
+     * @see <a href="http://jira.magnolia-cms.com/browse/MGNLUI-3220">MGNLUI-3220</a>
+     */
+    private void restoreVersion(Node node, String versionName, boolean createVersionBeforeRestore) throws RepositoryException {
+        final Version versionToRestore = versionManager.getVersion(node, versionName);
+
+        // Create another version before restore
+        if (createVersionBeforeRestore) {
+            createVersionBeforeRestore(node);
+        }
+
+        // Restore version
+        versionManager.restore(node, versionToRestore, true);
+
+        eventBus.fireEvent(new ContentChangedEvent(nodeAdapter.getItemId()));
+        uiContext.openNotification(MessageStyleTypeEnum.INFO, true, i18n.translate("ui-contentapp.actions.restoreVersion.notification.success"));
+
+        formDialogPresenter.closeDialog();
+
+        log.debug("Restored version [{}]", versionName);
     }
 
 }
--- magnolia-ui-contentapp/src/test/java/info/magnolia/ui/contentapp/browser/action/RestoreVersionActionTest.java
+++ magnolia-ui-contentapp/src/test/java/info/magnolia/ui/contentapp/browser/action/RestoreVersionActionTest.java
@@ -33,9 +33,11 @@
  */
 package info.magnolia.ui.contentapp.browser.action;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import info.magnolia.cms.beans.config.VersionConfig;
 import info.magnolia.cms.core.version.VersionManager;
 import info.magnolia.cms.security.operations.AccessDefinition;
 import info.magnolia.cms.security.operations.ConfiguredAccessDefinition;
@@ -58,8 +60,10 @@ import info.magnolia.ui.vaadin.integration.jcr.JcrNodeAdapter;
 
 import javax.inject.Named;
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.version.Version;
+import javax.jcr.version.VersionIterator;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -85,12 +89,19 @@ public class RestoreVersionActionTest extends RepositoryTestCase {
     private EventBus eventBus;
     private SimpleTranslator i18n;
 
+    private VersionManager versionManager;
+    private VersionConfig versionConfig;
+
     @Override
     @Before
     public void setUp() throws Exception {
         super.setUp();
+
         ComponentsTestUtil.setImplementation(AccessDefinition.class, ConfiguredAccessDefinition.class);
         ComponentsTestUtil.setImplementation(AvailabilityDefinition.class, ConfiguredAvailabilityDefinition.class);
+
+        versionManager = VersionManager.getInstance();
+        versionConfig = new VersionConfig();
         definition = new RestoreVersionActionDefinition();
 
         formDialogPresenter = mock(FormDialogPresenter.class);
@@ -111,8 +122,7 @@ public class RestoreVersionActionTest extends RepositoryTestCase {
     @Test
     public void testRestoreVersion() throws Exception {
         // GIVEN
-        VersionManager versionMan = VersionManager.getInstance();
-        versionMan.addVersion(node);
+        versionManager.addVersion(node);
 
         // change test node before restore
         node.setProperty("mgnl:template", "section");
@@ -122,8 +132,7 @@ public class RestoreVersionActionTest extends RepositoryTestCase {
         assertEquals("section", node.getProperty("mgnl:template").getString());
         assertFalse(node.hasNode("areaSubNode"));
 
-        AbstractJcrNodeAdapter item = new JcrNodeAdapter(node);
-        MockRestoreVersionAction restoreVersionAction = new MockRestoreVersionAction(definition, null, null, uiContext, formDialogPresenter, item, i18n, versionMan, eventBus);
+        MockRestoreVersionAction restoreVersionAction = new MockRestoreVersionAction(definition, null, null, uiContext, formDialogPresenter, new JcrNodeAdapter(node), i18n, versionManager, eventBus, versionConfig);
         restoreVersionAction.execute();
 
         // WHEN
@@ -138,26 +147,24 @@ public class RestoreVersionActionTest extends RepositoryTestCase {
     @Test
     public void testCheckVersionCreatedBeforeRestore() throws Exception {
         // GIVEN
-        VersionManager versionMan = VersionManager.getInstance();
-        versionMan.addVersion(node);
+        versionManager.addVersion(node);
 
         // change test node before restore
         node.setProperty("mgnl:template", "section");
         node.getNode("areaSubNode").remove();
         node.getSession().save();
 
-        assertEquals(2, versionMan.getAllVersions(node).getSize());
+        assertEquals(2, versionManager.getAllVersions(node).getSize());
 
-        AbstractJcrNodeAdapter item = new JcrNodeAdapter(node);
-        MockRestoreVersionAction restoreVersionAction = new MockRestoreVersionAction(definition, null, null, uiContext, formDialogPresenter, item, i18n, versionMan, eventBus);
+        MockRestoreVersionAction restoreVersionAction = new MockRestoreVersionAction(definition, null, null, uiContext, formDialogPresenter, new JcrNodeAdapter(node), i18n, versionManager, eventBus, versionConfig);
         restoreVersionAction.execute();
 
         // WHEN
         restoreVersionAction.getEditorCallback().onSuccess("");
 
         // THEN
-        assertEquals(3, versionMan.getAllVersions(node).getSize());
-        Version version =versionMan.getVersion(node,"1.1");
+        assertEquals(3, versionManager.getAllVersions(node).getSize());
+        Version version = versionManager.getVersion(node, "1.1");
         assertEquals(CREATED_VERSION_BEFORE_RESTORE, NodeTypes.Versionable.getComment(version));
         assertEquals("section", version.getProperty("mgnl:template").getString());
         assertFalse(version.hasNode("areaSubNode"));
@@ -166,40 +173,144 @@ public class RestoreVersionActionTest extends RepositoryTestCase {
     @Test
     public void testDoNotCreateVersionBeforeRestoreIfNotAllowed() throws Exception {
         // GIVEN
-        VersionManager versionMan = VersionManager.getInstance();
-        versionMan.addVersion(node);
+        versionManager.addVersion(node);
 
         // change test node before restore
         node.setProperty("mgnl:template", "section");
         node.getNode("areaSubNode").remove();
         node.getSession().save();
 
-        assertEquals(2, versionMan.getAllVersions(node).getSize());
+        assertEquals(2, versionManager.getAllVersions(node).getSize());
 
         definition.setCreateVersionBeforeRestore(false);
 
-        AbstractJcrNodeAdapter item = new JcrNodeAdapter(node);
-        MockRestoreVersionAction restoreVersionAction = new MockRestoreVersionAction(definition, null, null, uiContext, formDialogPresenter, item, i18n, versionMan, eventBus);
+        MockRestoreVersionAction restoreVersionAction = new MockRestoreVersionAction(definition, null, null, uiContext, formDialogPresenter, new JcrNodeAdapter(node), i18n, versionManager, eventBus, versionConfig);
         restoreVersionAction.execute();
 
         // WHEN
         restoreVersionAction.getEditorCallback().onSuccess("");
 
         // THEN
-        assertEquals(2, versionMan.getAllVersions(node).getSize());
+        assertEquals(2, versionManager.getAllVersions(node).getSize());
     }
 
+    /**
+     * We expect the second confirmation to be shown, as we're restoring the oldest version. However the restore will
+     * not trigger the creation of another version just before the restore itself.
+     *
+     * @see <a href="http://jira.magnolia-cms.com/browse/MGNLUI-3220">MGNLUI-3220</a>
+     */
+    @Test
+    public void testRestoreVersionWhenMultipleVersionsExistsAndRestoreOldest() throws Exception {
+        // GIVEN
+
+        // This Version will go as the version store will be full
+        createVersion(node, "prop", "test 1"); // 1.0
+        assertThat(versionManager.getAllVersions(node).getSize(), is(2L));
+
+        // This is the version we are going to restore (the oldest one)
+        Version version1_1 = createVersion(node, "prop", "test 2"); // 1.1
+        assertThat(versionManager.getAllVersions(node).getSize(), is(3L));
+
+        // Add another version to fill the version store
+        Version version1_2 = createVersion(node, "prop", "test 3"); // 1.2
+        assertThat(versionManager.getAllVersions(node).getSize(), is(4L));
+
+        // Add another version to fill the version store
+        Version version1_3 = createVersion(node, "prop", "test 4"); // 1.3
+        assertThat(versionManager.getAllVersions(node).getSize(), is(4L));
+
+        MockRestoreVersionAction restoreVersionAction = new MockRestoreVersionAction(definition, null, null, uiContext, formDialogPresenter, new JcrNodeAdapter(node), i18n, versionManager, eventBus, versionConfig);
+        restoreVersionAction.setSelectedVersion(version1_1.getName());
+        restoreVersionAction.execute();
+
+        restoreVersionAction.getEditorCallback().onSuccess(""); // First callback confirm
+
+        // Test before
+        VersionIterator versionIterator = versionManager.getAllVersions(node);
+        assertThat(node.getProperty("prop").getString(), is("test 4")); // no restore yet
+        assertThat(versionIterator.getSize(), is(4L));
+        // Versions store shouldn't change
+        assertThat(versionIterator.nextVersion().getName(), is("jcr:rootVersion"));
+        assertThat(versionIterator.nextVersion().getName(), is(version1_1.getName()));
+        assertThat(versionIterator.nextVersion().getName(), is(version1_2.getName()));
+        assertThat(versionIterator.nextVersion().getName(), is(version1_3.getName()));
+
+        // WHEN
+        restoreVersionAction.getConfirmationCallback().onSuccess(); // Second callback confirm
+
+        // THEN
+        // Test after
+        versionIterator = versionManager.getAllVersions(node);
+        assertThat(node.getProperty("prop").getString(), is("test 2")); // 1.1 was restored
+        assertThat(versionIterator.getSize(), is(4L));
+        // Versions store shouldn't change
+        assertThat(versionIterator.nextVersion().getName(), is("jcr:rootVersion"));
+        assertThat(versionIterator.nextVersion().getName(), is(version1_1.getName()));
+        assertThat(versionIterator.nextVersion().getName(), is(version1_2.getName()));
+        assertThat(versionIterator.nextVersion().getName(), is(version1_3.getName()));
+    }
+
+    /**
+     * We expect the second confirmation to not be shown, as we're not restoring the oldest version.
+     */
+    @Test
+    public void testRestoreVersionWhenMultipleVersionsExistsAndRestoreMiddle() throws Exception {
+        // GIVEN
+
+        // This Version will go as the version store will be full
+        createVersion(node, "prop", "test 1"); // 1.0
+        assertThat(versionManager.getAllVersions(node).getSize(), is(2L));
+
+        // This version will be replace with the version created before restore
+        createVersion(node, "prop", "test 2"); // 1.1
+        assertThat(versionManager.getAllVersions(node).getSize(), is(3L));
+
+        // This version will be restored
+        Version version1_2 = createVersion(node, "prop", "test 3"); // 1.2
+        assertThat(versionManager.getAllVersions(node).getSize(), is(4L));
+
+        // This version should stay is the version store
+        Version version1_3 = createVersion(node, "prop", "test 4"); // 1.3
+        assertThat(versionManager.getAllVersions(node).getSize(), is(4L));
+
+        MockRestoreVersionAction restoreVersionAction = new MockRestoreVersionAction(definition, null, null, uiContext, formDialogPresenter, new JcrNodeAdapter(node), i18n, versionManager, eventBus, versionConfig);
+        restoreVersionAction.setSelectedVersion(version1_2.getName());
+        restoreVersionAction.execute();
+
+        // WHEN
+        restoreVersionAction.getEditorCallback().onSuccess("");
+
+        // THEN
+        VersionIterator versionIterator = versionManager.getAllVersions(node);
+        assertThat(node.getProperty("prop").getString(), is("test 3"));
+        assertThat(versionIterator.getSize(), is(4L));
+        assertThat(versionIterator.nextVersion().getName(), is("jcr:rootVersion"));
+        assertThat(versionIterator.nextVersion().getName(), is(version1_2.getName()));
+        assertThat(versionIterator.nextVersion().getName(), is(version1_3.getName()));
+        assertThat(versionIterator.nextVersion().getName(), is("1.4")); // Version created before restore
+    }
+
+    private Version createVersion(final Node node, final String property, final String value) throws RepositoryException {
+        node.setProperty(property, value);
+        node.getSession().save();
+
+        return versionManager.addVersion(node);
+    }
+
+    /**
+     * Test class of {@link RestoreVersionAction}.
+     */
     private class MockRestoreVersionAction extends RestoreVersionAction {
 
         private String version = "1.0";
 
-        public MockRestoreVersionAction(RestoreVersionActionDefinition definition, AppContext appContext, LocationController locationController, UiContext uiContext, FormDialogPresenter formDialogPresenter, AbstractJcrNodeAdapter nodeAdapter, SimpleTranslator i18n, VersionManager versionManager, @Named(AdmincentralEventBus.NAME) EventBus eventBus) {
-            super(definition, appContext, locationController, uiContext, formDialogPresenter, nodeAdapter, i18n, versionManager, eventBus);
+        public MockRestoreVersionAction(RestoreVersionActionDefinition definition, AppContext appContext, LocationController locationController, UiContext uiContext, FormDialogPresenter formDialogPresenter, AbstractJcrNodeAdapter nodeAdapter, SimpleTranslator i18n, VersionManager versionManager, @Named(AdmincentralEventBus.NAME) EventBus eventBus, VersionConfig versionConfig) {
+            super(definition, appContext, locationController, uiContext, formDialogPresenter, nodeAdapter, i18n, versionManager, eventBus, versionConfig);
         }
 
         @Override
         protected BeanItem<?> getItem() {
-
             BeanItem<?> item = super.getItem();
             Property property = new TextField();
             property.setValue(version);
@@ -207,7 +318,7 @@ public class RestoreVersionActionTest extends RepositoryTestCase {
             return item;
         }
 
-        protected void setSelectedVersion(String version){
+        protected void setSelectedVersion(String version) {
             this.version = version;
         }
     }
