55133f8315590ae84689e3d0c6ca8525bac2eb8e
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2014-10-30 11:57:53 +0200

    MGNLUI-3124 getting user node via UUID provided by User object.

--- security-app/src/main/java/info/magnolia/security/app/dialog/action/SaveUserDialogAction.java
+++ security-app/src/main/java/info/magnolia/security/app/dialog/action/SaveUserDialogAction.java
@@ -132,7 +132,7 @@ public class SaveUserDialogAction extends SaveDialogAction<SaveUserDialogActionD
                 parentNode.getSession().checkPermission(parentNode.getPath(), Session.ACTION_ADD_NODE);
 
                 user = userManager.createUser(parentPath, newUserName, newPassword);
-                userNode = parentNode.getNode(newUserName);
+                userNode = session.getNodeByIdentifier(user.getIdentifier());
             } else {
                 userNode = userItem.getJcrItem();
                 String existingUserName = userNode.getName();
