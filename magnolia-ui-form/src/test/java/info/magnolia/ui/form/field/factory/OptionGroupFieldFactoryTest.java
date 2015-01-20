ec14ae2e1e6950cc84498413782ffc98cb9993e1
Evzen Fochr <evzen.fochr@magnolia-cms.com>
2015-01-20 16:07:37 +0100

    Revert "MGNLUI-2397 for OptionGroupFieldFactory default value is set even if setNewItemsAllowed(true), null is set if no item is preselected"
    This reverts commit 25919d67f3436b4899a9783d5d835084b489183a.

--- magnolia-ui-form/src/test/java/info/magnolia/ui/form/field/factory/OptionGroupFieldFactoryTest.java
+++ magnolia-ui-form/src/test/java/info/magnolia/ui/form/field/factory/OptionGroupFieldFactoryTest.java
@@ -34,7 +34,6 @@
 package info.magnolia.ui.form.field.factory;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
 
 import info.magnolia.test.mock.MockComponentProvider;
 import info.magnolia.ui.form.field.definition.OptionGroupFieldDefinition;
@@ -98,19 +97,6 @@ public class OptionGroupFieldFactoryTest extends AbstractFieldFactoryTestCase<Op
     public void testSelectedRadioFieldOnPreexistingNodeWithNullValue() throws Exception {
         // GIVEN
         // we keep baseItem as a regular JcrNodeAdapter here
-        dialogSelect = new OptionGroupFieldFactory(definition, baseItem, new MockComponentProvider());
-
-        // WHEN
-        Field field = dialogSelect.createField();
-
-        // THEN
-        assertNull(field.getValue());
-    }
-
-    @Test
-    public void testSelectedRadioFieldOnPreexistingNodeWithPreselectedValue() throws Exception {
-        // GIVEN
-        // we keep baseItem as a regular JcrNodeAdapter here and preselect of items
         definition.getOptions().get(2).setSelected(true);
         dialogSelect = new OptionGroupFieldFactory(definition, baseItem, new MockComponentProvider());
 
@@ -118,7 +104,7 @@ public class OptionGroupFieldFactoryTest extends AbstractFieldFactoryTestCase<Op
         Field field = dialogSelect.createField();
 
         // THEN
-        assertEquals(definition.getOptions().get(2).getValue(),field.getValue().toString());
+        assertNull(field.getValue());
     }
 
     @Test
