3629cc9401d20fbc4b784e3ff885288bccba5150
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2015-02-05 12:03:39 +0200

    MGNLUI-3338 MediaEditorRegistry ported to new Magnolia 5.4 configuration API.
     - fixed id handling in MediaEditorRegistry and binding MediaEditorRegistry with an appropriate path.

--- magnolia-ui-mediaeditor/src/main/java/info/magnolia/ui/mediaeditor/registry/MediaEditorDefinitionType.java
+++ magnolia-ui-mediaeditor/src/main/java/info/magnolia/ui/mediaeditor/registry/MediaEditorDefinitionType.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2013-2015 Magnolia International
+ * This file Copyright (c) 2015 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
@@ -33,21 +33,25 @@
  */
 package info.magnolia.ui.mediaeditor.registry;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import info.magnolia.config.registry.DefinitionType;
+import info.magnolia.ui.mediaeditor.definition.MediaEditorDefinition;
 
 /**
- * Populates {@link MediaEditorRegistry} by scanning the nodes in the config workspace.
- *
- * @deprecated since 5.4, replaced by {@link info.magnolia.config.source.jcr.RegistryBasedObservingManager RegistryBasedObservingManager},
- * within {@link info.magnolia.config.source.jcr.JcrConfigurationSource JcrConfigurationSource}
+ * Media editor-specific {@link DefinitionType}.
  */
-@Deprecated
-public class ConfiguredMediaEditorDefinitionManager {
+public class MediaEditorDefinitionType implements DefinitionType {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    public static MediaEditorDefinitionType TYPE = new MediaEditorDefinitionType();
 
-    public void start() {
-        log.warn("ConfiguredFieldTypeDefinitionManager is deprecated. It will be revived to provide backwards compatibility, but should not be used anymore.");
+    private MediaEditorDefinitionType() {}
+
+    @Override
+    public String name() {
+        return "mediaEditor";
+    }
+
+    @Override
+    public Class baseClass() {
+        return MediaEditorDefinition.class;
     }
 }
