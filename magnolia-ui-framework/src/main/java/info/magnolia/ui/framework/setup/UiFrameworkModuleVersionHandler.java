e0e33ba7643d22fce74e68edda29c2c622f254a8
Evzen Fochr <evzen.fochr@magnolia-cms.com>
2015-08-24 11:23:07 +0200

    MGNLUI-3529 versionHandler, check if properties do not exist yet

--- magnolia-ui-framework/src/main/java/info/magnolia/ui/framework/setup/UiFrameworkModuleVersionHandler.java
+++ magnolia-ui-framework/src/main/java/info/magnolia/ui/framework/setup/UiFrameworkModuleVersionHandler.java
@@ -49,6 +49,7 @@ import info.magnolia.module.delta.MoveNodeTask;
 import info.magnolia.module.delta.NewPropertyTask;
 import info.magnolia.module.delta.NodeExistsDelegateTask;
 import info.magnolia.module.delta.PartialBootstrapTask;
+import info.magnolia.module.delta.PropertyExistsDelegateTask;
 import info.magnolia.module.delta.RemoveNodeTask;
 import info.magnolia.module.delta.RenameNodesTask;
 import info.magnolia.module.delta.SetPropertyTask;
@@ -161,8 +162,10 @@ public class UiFrameworkModuleVersionHandler extends DefaultModuleVersionHandler
         register(DeltaBuilder.update("5.4.2", "")
                 .addTask(new NodeExistsDelegateTask("Check if path '/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name' exist.", "/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name",
                                 new ArrayDelegateTask("",
-                                        new NewPropertyTask("Add fallback MimeType to upload zip action, that will be used if browser do not recognize mimeType file has allowed extension.", "/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name", "fallbackMimeType", "application/zip"),
-                                        new NewPropertyTask("Add allowed extension that will be test against, if browser do not recognize mimeType of file.", "/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name", "allowedFileExtensionPattern", ".*\\.(zip)$"),
+                                        new PropertyExistsDelegateTask("Check if property 'fallbackMimeType' do not exist.", "/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name", "fallbackMimeType", null,
+                                                new NewPropertyTask("Add fallback MimeType to upload zip action, that will be used if browser do not recognize mimeType file has allowed extension.", "/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name", "fallbackMimeType", "application/zip")),
+                                        new PropertyExistsDelegateTask("Check if property 'allowedFileExtensionPattern' do not exist.", "/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name", "allowedFileExtensionPattern", null,
+                                                new NewPropertyTask("Add allowed extension that will be test against, if browser do not recognize mimeType of file.", "/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name", "allowedFileExtensionPattern", ".*\\.(zip)$")),
                                         new CheckAndModifyPropertyValueTask("Add mimeType application/octet-stream that is used by google chrome for zip files.", "Add mimeType application/octet-stream that is used by google chrome for zip files.", RepositoryConstants.CONFIG, "/modules/ui-framework/dialogs/importZip/form/tabs/import/fields/name", "allowedMimeTypePattern", "application/(zip|x-zip|x-zip-compressed)", "application/(zip|x-zip|x-zip-compressed|octet-stream)")))
                 ));
     }
