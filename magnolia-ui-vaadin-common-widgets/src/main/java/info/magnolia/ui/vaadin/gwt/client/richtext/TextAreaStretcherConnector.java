ffba91b79db3d7df004c4aef3c289e0b308ea67f
Sang Ngo <sang.ngo@ext.magnolia-cms.com>
2015-11-02 11:26:40 +0700

    MGNLUI-3604: Show dialog buttons when text area field is maximized. When clicking on (?) button, or errorAmounts is changed, the full-height text will be minimized.

--- magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/gwt/client/richtext/TextAreaStretcherConnector.java
+++ magnolia-ui-vaadin-common-widgets/src/main/java/info/magnolia/ui/vaadin/gwt/client/richtext/TextAreaStretcherConnector.java
@@ -70,44 +70,64 @@ public class TextAreaStretcherConnector extends AbstractExtensionConnector {
     public static final String COLLAPSED = "collapsed";
     public static final String CKEDITOR_TOOLBOX = ".cke_top";
     public static final String TEXTAREA_STRETCHED = "textarea-stretched";
-
     public static final String RICH_TEXT_STYLE_NAME = "rich-text";
     public static final String SIMPLE_STYLE_NAME = "simple";
-
     public static final int DELAY_MS = 500;
+
+    private static final int UNCALCULATED_SIZE = -1;
+    private static final int TOP = 0;
+    private static final int RIGHT = 1;
+    private static final int BOTTOM = 2;
+    private static final int LEFT = 3;
 
     private Widget form;
     private Widget dialog;
     private Widget textWidget;
-
     private Element stretchControl = DOM.createDiv();
-    private WindowResizeListener windowResizeListener = new WindowResizeListener();
+    private ComputedStyle dialogComputedStyle;
 
+    // Border, padding, margin property for dialog-wrapper, dialog-header, dialog-content class, contain values for top, right, bottom, left.
+    private int[] dialogWrapperBorder;
+    private int[] dialogHeaderPadding;
+    private int[] dialogContentMargin;
+    private int[] dialogMargin;
+    private int[] dialogPadding;
+    private String formHeight;
+    private int dialogHeaderOuterHeight = UNCALCULATED_SIZE;
+    private int dialogFooterOuterHeight = UNCALCULATED_SIZE;
+    private int dialogDesErrorOuterHeight = UNCALCULATED_SIZE;
     private boolean isOverlay = false;
     private boolean isRichTextEditor = false;
 
     private StateChangeEvent.StateChangeHandler textAreaSizeHandler = new StateChangeEvent.StateChangeHandler() {
         @Override
         public void onStateChanged(StateChangeEvent stateChangeEvent) {
-            stretchTextArea(textWidget.getElement().getStyle());
+            if (isFormVisible()) {
+                adjustTextAreaAndFormSizeToScreen();
+            }
         }
     };
+
+    private WindowResizeListener windowResizeListener = new WindowResizeListener();
 
     private ElementResizeListener formResizeListener = new ElementResizeListener() {
         @Override
         public void onElementResize(ElementResizeEvent e) {
-            if (isRichTextEditor) {
-                Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-                    @Override
-                    public void execute() {
-                        updateSize();
-                    }
-                });
-            } else {
-                updateSize();
+            doElementResize();
+        }
+    };
+
+    private StateChangeEvent.StateChangeHandler formStateChangeHandler = new StateChangeEvent.StateChangeHandler() {
+        @Override
+        public void onStateChanged(StateChangeEvent stateChangeEvent) {
+            if (stateChangeEvent.hasPropertyChanged("descriptionsVisible") || stateChangeEvent.hasPropertyChanged("errorAmount")) {
+                if (!getState().isCollapsed) {
+                    unregisterSizeChangeListeners();
+                    getRpcProxy(TextAreaStretcherServerRpc.class).toggle(textWidget.getOffsetWidth(), textWidget.getOffsetHeight());
+                }
             }
         }
-    };;
+    };
 
     @Override
     public void onStateChanged(StateChangeEvent stateChangeEvent) {
@@ -198,10 +218,15 @@ public class TextAreaStretcherConnector extends AbstractExtensionConnector {
         final ComponentConnector formConnector = Util.findConnectorFor(this.form);
         if (formConnector != null) {
             formConnector.getLayoutManager().addElementResizeListener(this.form.getElement(), formResizeListener);
+            formConnector.addStateChangeHandler(formStateChangeHandler);
         }
     }
 
     private void updateSize() {
+        if (!isFormVisible()) {
+            return;
+        }
+
         if (!getState().isCollapsed) {
             stretchControl.replaceClassName("icon-open-fullscreen-2", "icon-close-fullscreen-2");
             stretchControl.replaceClassName(COLLAPSED, STRETCHED);
@@ -209,24 +234,18 @@ public class TextAreaStretcherConnector extends AbstractExtensionConnector {
 
             Style style = textWidget.getElement().getStyle();
             style.setPosition(Style.Position.ABSOLUTE);
-            Element header = getDialogHeaderElement();
-            ComputedStyle headerCS = new ComputedStyle(header);
+            style.setZIndex(5);
 
-            int top = form.getAbsoluteTop() - dialog.getAbsoluteTop();
-            top = isOverlay ? top : top + headerCS.getPadding()[0] + headerCS.getPadding()[2];
-
-            int left = isOverlay ? 0 : form.getAbsoluteLeft();
+            int top = calculateTextWidgetTop();
+            int left = calculateTextWidgetLeft();
 
             style.setLeft(left, Style.Unit.PX);
             style.setTop(top, Style.Unit.PX);
 
-            stretchTextArea(style);
-            style.setZIndex(5);
+            adjustTextAreaAndFormSizeToScreen();
 
-            if (!isOverlay && !isRichTextEditor) {
-                stretchControl.getStyle().setTop(top + 5, Style.Unit.PX);
-                stretchControl.getStyle().setLeft(left + textWidget.getOffsetWidth() - stretchControl.getOffsetWidth() - 5, Style.Unit.PX);
-
+            if (!isRichTextEditor) {
+                setStretchControlPosition(top, left);
             }
 
             hideOtherStretchers();
@@ -234,6 +253,8 @@ public class TextAreaStretcherConnector extends AbstractExtensionConnector {
             stretchControl.replaceClassName(STRETCHED, COLLAPSED);
             stretchControl.replaceClassName("icon-close-fullscreen-2", "icon-open-fullscreen-2");
             form.asWidget().removeStyleName(TEXTAREA_STRETCHED);
+            // Restore form height
+            form.setHeight(formHeight);
             clearTraces();
         }
     }
@@ -242,6 +263,20 @@ public class TextAreaStretcherConnector extends AbstractExtensionConnector {
     public void onUnregister() {
         super.onUnregister();
         clearTraces();
+    }
+
+    private void setStretchControlPosition(int top, int left) {
+        stretchControl.getStyle().setTop(top + 5, Style.Unit.PX);
+        stretchControl.getStyle().setLeft(left + textWidget.getOffsetWidth() - stretchControl.getOffsetWidth() - 5, Style.Unit.PX);
+    }
+
+    private int calculateTextWidgetTop() {
+        int top = getDialogHeaderOuterHeight() + getDialogDesErrorOuterHeight();
+        return isOverlay ? top + getDialogMargin()[TOP] + getDialogPadding()[TOP] : top + getDialogHeaderPadding()[TOP] + getDialogHeaderPadding()[BOTTOM];
+    }
+
+    private int calculateTextWidgetLeft() {
+        return isOverlay ? getDialogPadding()[LEFT] : form.getAbsoluteLeft();
     }
 
     private void hideOtherStretchers() {
@@ -258,13 +293,9 @@ public class TextAreaStretcherConnector extends AbstractExtensionConnector {
 
         stretchControl.getStyle().clearTop();
         stretchControl.getStyle().clearLeft();
+        stretchControl.getStyle().clearDisplay();
 
         JQueryWrapper.select("." + STRETCHER_BASE).setCss("display", "");
-    }
-
-    private void stretchTextArea(Style style) {
-        style.setWidth(form.getOffsetWidth(), Style.Unit.PX);
-        adjustTextAreaHeightToScreen(getConnection().getUIConnector().getWidget().getOffsetHeight());
     }
 
     private void unregisterSizeChangeListeners() {
@@ -278,11 +309,8 @@ public class TextAreaStretcherConnector extends AbstractExtensionConnector {
         final ComponentConnector formConnector = Util.findConnectorFor(this.form);
         if (formConnector != null) {
             formConnector.getLayoutManager().removeElementResizeListener(this.form.getElement(), formResizeListener);
+            formConnector.removeStateChangeHandler(formStateChangeHandler);
         }
-    }
-
-    private Element getDialogHeaderElement() {
-        return JQueryWrapper.select(dialog.asWidget()).find(".dialog-header").get(0);
     }
 
     private void checkOverlay() {
@@ -303,17 +331,147 @@ public class TextAreaStretcherConnector extends AbstractExtensionConnector {
             it = it.getParent();
         }
         this.form = (it instanceof FormView) ? it : null;
+        // Keep original form's height
+        this.formHeight = this.form.getElement().getStyle().getHeight();
     }
 
-    private void adjustTextAreaHeightToScreen(int uiHeight) {
-        int formTop = form.getAbsoluteTop();
-        textWidget.setHeight((uiHeight - formTop) + "px");
+    private void adjustTextAreaAndFormSizeToScreen() {
+        int formHeight = calculateFormHeight();
+        form.setHeight(formHeight + "px");
+        textWidget.setWidth((form.getOffsetWidth() + getDialogWrapperBorder()[RIGHT] + getDialogWrapperBorder()[LEFT]) + "px");
+        textWidget.setHeight(formHeight + "px");
+    }
+
+    private int calculateFormHeight() {
+        int formHeight = dialog.getOffsetHeight();
+        formHeight -= getDialogHeaderOuterHeight() + getDialogFooterOuterHeight();
+        formHeight -= getDialogMargin()[TOP];
+        formHeight -= getDialogContentMargin()[TOP] - getDialogContentMargin()[BOTTOM];
+        formHeight -= getDialogDesErrorOuterHeight();
+
+        return formHeight;
+    }
+
+    private void doElementResize() {
+        if (isRichTextEditor) {
+            Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+                @Override
+                public void execute() {
+                    updateSize();
+                }
+            });
+        } else {
+            updateSize();
+        }
+    }
+
+    private int getElementsOuterHeight(String... elementClasses) {
+        int elementsOuterHeight = 0;
+        for (String clazzName : elementClasses) {
+            Element element = JQueryWrapper.select(dialog.asWidget()).find(clazzName).get(0);
+            if (element != null) {
+                int margin[] = new ComputedStyle(element).getMargin();
+                elementsOuterHeight += element.getOffsetHeight() + margin[TOP] + margin[BOTTOM];
+            }
+        }
+        return elementsOuterHeight;
+    }
+
+    private int[] getDialogHeaderPadding() {
+        if (dialogHeaderPadding == null) {
+            Element headerElement = JQueryWrapper.select(dialog.asWidget()).find(".dialog-header").get(0);
+            if (headerElement != null) {
+                dialogHeaderPadding = new ComputedStyle(headerElement).getPadding();
+            } else {
+                dialogHeaderPadding = new int[]{0, 0, 0, 0};
+            }
+        }
+
+        return dialogHeaderPadding;
+    }
+
+    private int[] getDialogContentMargin() {
+        if (dialogContentMargin == null) {
+            Element contentElement = JQueryWrapper.select(dialog.asWidget()).find(".dialog-content").get(0);
+            if (contentElement != null) {
+                dialogContentMargin = new ComputedStyle(contentElement).getMargin();
+            } else {
+                dialogContentMargin = new int[]{0, 0, 0, 0};
+            }
+        }
+
+        return dialogContentMargin;
+    }
+
+    private int[] getDialogPadding() {
+        if (dialogPadding == null) {
+            dialogPadding = getDialogComputedStyle().getPadding();
+        }
+
+        return dialogPadding;
+    }
+
+    private int[] getDialogMargin() {
+        if (dialogMargin == null) {
+            dialogMargin = getDialogComputedStyle().getMargin();
+        }
+
+        return dialogMargin;
+    }
+
+    private int[] getDialogWrapperBorder() {
+        if (dialogWrapperBorder == null) {
+            Element dialogWrapperElement = JQueryWrapper.select(dialog.asWidget()).find(".dialog-wrapper").get(0);
+            if (dialogWrapperElement != null) {
+                dialogWrapperBorder = new ComputedStyle(dialogWrapperElement).getBorder();
+            } else {
+                dialogWrapperBorder = new int[]{0, 0, 0, 0};
+            }
+        }
+
+        return dialogWrapperBorder;
+    }
+
+    private int getDialogFooterOuterHeight() {
+        if (dialogFooterOuterHeight == UNCALCULATED_SIZE) {
+            dialogFooterOuterHeight = getElementsOuterHeight(".dialog-footer");
+        }
+
+        return dialogFooterOuterHeight;
+    }
+
+    private int getDialogHeaderOuterHeight() {
+        if (dialogHeaderOuterHeight == UNCALCULATED_SIZE) {
+            dialogHeaderOuterHeight = getElementsOuterHeight(".dialog-header");
+        }
+
+        return dialogHeaderOuterHeight;
+    }
+
+    private int getDialogDesErrorOuterHeight() {
+        if (dialogDesErrorOuterHeight == UNCALCULATED_SIZE) {
+            dialogDesErrorOuterHeight = getElementsOuterHeight(".dialog-description", ".dialog-error");
+        }
+
+        return dialogDesErrorOuterHeight;
+    }
+
+    private ComputedStyle getDialogComputedStyle() {
+        if (dialogComputedStyle == null) {
+            dialogComputedStyle = new ComputedStyle(dialog.getElement());
+        }
+
+        return dialogComputedStyle;
+    }
+
+    private boolean isFormVisible() {
+        return JQueryWrapper.select(form.asWidget()).is(":visible");
     }
 
     private class WindowResizeListener implements ElementResizeListener {
         @Override
         public void onElementResize(ElementResizeEvent e) {
-            adjustTextAreaHeightToScreen(e.getLayoutManager().getOuterHeight(e.getElement()));
+            doElementResize();
         }
     }
 
