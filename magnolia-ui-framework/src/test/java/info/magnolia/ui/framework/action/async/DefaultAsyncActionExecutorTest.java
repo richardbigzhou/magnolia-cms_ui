b36c6c6cbd61ef0c52faf9f25399caaeff95b9bd
Mikaël Geljić <mikael.geljic@magnolia-cms.com>
2015-11-10 13:06:51 +0100

    QA Clean up AbstractCommandActionTest and DefaultAsyncExecutorTest
    * Get rid of unnecessary components in #setUp
    * Initialize Context, Session and Definition in #setUp (removing redundant instantiations)
    * Make use of the session field, rather than going through MgnlContext again
    * Test action #execute (rather than #onPreExecute, which only gives a partial representation of the execution, and requires unnecessary stubbing)
    * Remove test method prefixes
    * Combine param assertions for existence and value (hasEntry matcher)
    * Make sure tests don't depend on running order (executeAsync, "parallel" setting messing up with jobName)
    * Shrink down silly execution time for async test with timeToWait
    
    * Move async scheduler-related test to DefaultAsyncExecutorTest
    * Remove redudant test for SchedulerException subtype
    * Introduce new test for when async executor completes before timeToWait

--- magnolia-ui-framework/src/test/java/info/magnolia/ui/framework/action/async/DefaultAsyncActionExecutorTest.java
+++ magnolia-ui-framework/src/test/java/info/magnolia/ui/framework/action/async/DefaultAsyncActionExecutorTest.java
@@ -33,96 +33,134 @@
  */
 package info.magnolia.ui.framework.action.async;
 
-
 import static com.google.common.collect.Maps.newHashMap;
 import static info.magnolia.test.hamcrest.ExceptionMatcher.instanceOf;
 import static info.magnolia.test.hamcrest.ExecutionMatcher.throwsAnException;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
 import info.magnolia.cms.security.User;
-import info.magnolia.context.Context;
+import info.magnolia.context.MgnlContext;
 import info.magnolia.i18nsystem.SimpleTranslator;
 import info.magnolia.module.scheduler.SchedulerModule;
+import info.magnolia.repository.RepositoryConstants;
+import info.magnolia.test.ComponentsTestUtil;
 import info.magnolia.test.hamcrest.Execution;
+import info.magnolia.test.mock.MockContext;
+import info.magnolia.test.mock.jcr.MockSession;
 import info.magnolia.ui.api.action.CommandActionDefinition;
-import info.magnolia.ui.api.context.UiContext;
 import info.magnolia.ui.vaadin.integration.jcr.JcrItemAdapter;
+import info.magnolia.ui.vaadin.integration.jcr.JcrNodeAdapter;
 
 import java.util.Map;
 
 import javax.inject.Provider;
-import javax.jcr.Item;
+import javax.jcr.Session;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.stubbing.answers.ReturnsArgumentAt;
 import org.quartz.JobDetail;
-import org.quartz.ObjectAlreadyExistsException;
+import org.quartz.JobExecutionContext;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 
+import com.google.common.collect.Lists;
+
 public class DefaultAsyncActionExecutorTest {
 
-    private DefaultAsyncActionExecutor<CommandActionDefinition> executor;
-    private JcrItemAdapter item;
+    private static final String TEST_USER = "phantomas";
+
+    private Session session;
+    private CommandActionDefinition definition;
+
     private Scheduler scheduler;
     private final Map<String, Object> params = newHashMap();
+    private SimpleTranslator i18n;
+    private Provider<SchedulerModule> schedulerModuleProvider;
+    private MockContext ctx;
+    private JcrItemAdapter item;
 
     @Before
     public void setUp() throws Exception {
-        CommandActionDefinition definition = mock(CommandActionDefinition.class);
-        given(definition.getCommand()).willReturn("TestCommand");
+        // Init context
+        session = new MockSession(RepositoryConstants.WEBSITE);
+        ctx = new MockContext();
+        ctx.addSession(RepositoryConstants.WEBSITE, session);
 
-        Provider<SchedulerModule> provider = mock(Provider.class);
-        Context context = mock(Context.class);
         User user = mock(User.class);
-        given(context.getUser()).willReturn(user);
-        given(user.getName()).willReturn("TestUser");
+        when(user.getName()).thenReturn(TEST_USER);
+        ctx.setUser(user);
+        MgnlContext.setInstance(ctx);
 
-        UiContext uiContext = mock(UiContext.class);
-        SimpleTranslator simpleTranslator = mock(SimpleTranslator.class);
-
-        Trigger trigger = mock(Trigger.class);
-        JobDetail jobDetail = mock(JobDetail.class);
-
+        // Mock scheduler
+        schedulerModuleProvider = mock(Provider.class);
         SchedulerModule schedulerModule = mock(SchedulerModule.class);
-        given(provider.get()).willReturn(schedulerModule);
+        given(schedulerModuleProvider.get()).willReturn(schedulerModule);
         scheduler = mock(Scheduler.class);
-        scheduler.scheduleJob(jobDetail, trigger);
         given(schedulerModule.getScheduler()).willReturn(scheduler);
 
-        item = mock(JcrItemAdapter.class);
+        definition = new CommandActionDefinition();
+        i18n = mock(SimpleTranslator.class, new ReturnsArgumentAt(0));
+        item = new JcrNodeAdapter(session.getRootNode());
+    }
 
-        Item jcrItem = mock(Item.class);
-
-        given(item.getJcrItem()).willReturn(jcrItem);
-        given(jcrItem.getPath()).willReturn("/foo/bar");
-
-        executor = new DefaultAsyncActionExecutor<>(definition, provider, context, uiContext, simpleTranslator);
+    @After
+    public void tearDown() throws Exception {
+        MgnlContext.setInstance(null);
+        session = null;
+        ComponentsTestUtil.clear();
     }
 
     @Test
-    public void schedulerExceptionIsCaught() throws Exception {
+    public void executeCompletesBeforeTimeToWait() throws Exception {
+        // GIVEN
+        definition.setAsynchronous(true);
+        definition.setCommand("qux");
+        // no mocking of scheduler#getCurrentlyExecutingJobs => does as if job had completed already
+
+        AsyncActionExecutor executor = new DefaultAsyncActionExecutor<>(definition, schedulerModuleProvider, ctx, null, i18n);
+
+        // WHEN
+        boolean isRunningInTheBackground = executor.execute(item, null);
+
+        // THEN
+        assertThat(isRunningInTheBackground, is(false));
+    }
+
+    @Test
+    public void executeTakesLongerThanTimeToWait() throws Exception {
+        // GIVEN
+        definition.setAsynchronous(true);
+        definition.setCommand("qux");
+        definition.setTimeToWait(500);
+        definition.setParallel(false); // parallel execution messes up with job names depending on test-order
+
+        JobExecutionContext jobExecutionContext = mock(JobExecutionContext.class);
+        JobDetail jobDetail = new JobDetail();
+        jobDetail.setName("UI Action triggered execution of [default:qux] by user [" + TEST_USER + "].");
+        when(jobExecutionContext.getJobDetail()).thenReturn(jobDetail);
+        when(scheduler.getCurrentlyExecutingJobs()).thenReturn(Lists.newArrayList(jobExecutionContext));
+
+        AsyncActionExecutor executor = new DefaultAsyncActionExecutor<>(definition, schedulerModuleProvider, ctx, null, i18n);
+
+        // WHEN
+        boolean isRunningInTheBackground = executor.execute(item, null);
+
+        // THEN
+        assertThat(isRunningInTheBackground, is(true));
+    }
+
+    @Test
+    public void executeCatchesSchedulerException() throws Exception {
         // GIVEN
         doThrow(SchedulerException.class).when(scheduler).scheduleJob(any(JobDetail.class), any(Trigger.class));
-
-
-        // WHEN / THEN
-        assertThat(new Execution() {
-            @Override
-            public void evaluate() throws Exception {
-                executor.execute(item, params);
-            }
-        }, throwsAnException(instanceOf(AsyncActionExecutor.ParallelExecutionException.class)));
-    }
-
-    @Test
-    public void objectAlreadyExistsExceptionIsCaught() throws Exception {
-        // GIVEN
-        doThrow(ObjectAlreadyExistsException.class).when(scheduler).scheduleJob(any(JobDetail.class), any(Trigger.class));
+        final AsyncActionExecutor executor = new DefaultAsyncActionExecutor<>(definition, schedulerModuleProvider, ctx, null, i18n);
 
         // WHEN / THEN
         assertThat(new Execution() {
